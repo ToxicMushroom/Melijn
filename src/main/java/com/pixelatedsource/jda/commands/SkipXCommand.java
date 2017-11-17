72011133cac2a6cb5dd4c6e18ea9e203c3d84595
BuildTools <=>
2017-11-17 22:29:17 +0100

    Bug fixes

--- src/main/java/com/pixelatedsource/jda/commands/SkipXCommand.java
+++ src/main/java/com/pixelatedsource/jda/commands/SkipXCommand.java
@@ -16,20 +16,18 @@ public class SkipXCommand extends Command {
 
     @Override
     protected void execute(CommandEvent event) {
-        String[] args = event.getArgs().replaceFirst(":"," ").split("\\s+");
-        MusicPlayer player = manager.getPlayer(event.getGuild());
-        int minutes;
+        String[] args = event.getArgs().replaceFirst(":", " ").split("\\s+");
         int seconds;
-        long time;
-        try {
-           minutes = Integer.parseInt(args[0]);
-           seconds = Integer.parseInt(args[1]);
+        MusicPlayer player = manager.getPlayer(event.getGuild());
+        if (args[0] == null || args[0].equalsIgnoreCase("")) args[0] = "0";
+        if (args.length < 2) seconds = 0; else try {
+            Integer.parseInt(args[0]);
+            seconds = Integer.parseInt(args[1]);
         } catch (NumberFormatException e) {
-            e.addSuppressed(e);
-            event.reply("Usage: " + PixelatedBot.PREFIX + this.name + " 1:");
+            event.reply("Usage: " + PixelatedBot.PREFIX + this.name + " 1:10");
+            e.addSuppressed(e.getCause());
             return;
         }
-        time = minutes * 60000 + seconds * 1000;
-        player.getAudioPlayer().getPlayingTrack().setPosition(time);
+        player.getAudioPlayer().getPlayingTrack().setPosition(Integer.parseInt(args[0]) * 60000 + seconds  * 1000);
     }
 }
