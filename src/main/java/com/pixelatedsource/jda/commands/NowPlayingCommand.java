9062164d9961c4c03124a52a5d1ece5419fbf763
Merlijn Verstraete <merlijn.verstraete@gmail.com>
2018-01-05 21:13:29 +0100

    Nieuwe simele command om niet elke keer queue te moeten gebruiken om te zien wat er wordt gespeeld

---
+++ src/main/java/com/pixelatedsource/jda/commands/NowPlayingCommand.java
@@ -0,0 +1,34 @@
+package com.pixelatedsource.jda.commands;
+
+import com.jagrosh.jdautilities.commandclient.Command;
+import com.jagrosh.jdautilities.commandclient.CommandEvent;
+import com.pixelatedsource.jda.Helpers;
+import com.pixelatedsource.jda.music.MusicManager;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.core.EmbedBuilder;
+
+public class NowPlayingCommand extends Command {
+
+    public NowPlayingCommand() {
+        this.name = "np";
+        this.aliases = new String[] {"nowplaying", "playing", "nplaying", "nowp"};
+        this.help = "Show you the song that the bot is playing";
+        this.guildOnly = true;
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        if (Helpers.hasPerm(event.getGuild().getMember(event.getAuthor()), this.name, 0)) {
+            AudioTrack track = MusicManager.getManagerinstance().getPlayer(event.getGuild()).getAudioPlayer().getPlayingTrack();
+            if (track == null)
+                event.reply("There are no songs playing at the moment.");
+            else
+                event.reply(new EmbedBuilder()
+                        .setTitle("NowPlaying")
+                        .setColor(Helpers.EmbedColor)
+                        .setDescription(track.getInfo().title + " `" + Helpers.getDurationBreakdown(track.getPosition()) +" / " +  Helpers.getDurationBreakdown(track.getInfo().length) + "`")
+                        .setFooter(Helpers.getFooterStamp(), Helpers.getFooterIcon())
+                        .build());
+        }
+    }
+}
