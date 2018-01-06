2c9692fcf512c44cf849909c01be0fbf018c84fa
Merlijn Verstraete <merlijn.verstraete@gmail.com>
2018-01-06 11:43:42 +0100

    New command to remove a song or songs out of a queue

---
+++ src/main/java/com/pixelatedsource/jda/commands/RemoveCommand.java
@@ -0,0 +1,120 @@
+package com.pixelatedsource.jda.commands;
+
+import com.jagrosh.jdautilities.commandclient.Command;
+import com.jagrosh.jdautilities.commandclient.CommandEvent;
+import com.pixelatedsource.jda.Helpers;
+import com.pixelatedsource.jda.music.MusicManager;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Guild;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.BlockingQueue;
+
+public class RemoveCommand extends Command {
+    public RemoveCommand() {
+        this.name = "remove";
+        this.aliases = new String[]{"delete"};
+        this.help = "Remove songs of the queue";
+        this.guildOnly = true;
+    }
+
+    private MusicManager manager = MusicManager.getManagerinstance();
+
+    @Override
+    protected void execute(CommandEvent event) {
+        if (Helpers.hasPerm(event.getGuild().getMember(event.getAuthor()), this.name, 0)) {
+            BlockingQueue<AudioTrack> tracks = manager.getPlayer(event.getGuild()).getListener().getTracks();
+            String[] args = event.getArgs().replaceAll("\\s+", "").split(",");
+            HashMap<Integer, AudioTrack> songs = new HashMap<>();
+            int i = 0;
+            for (AudioTrack track : tracks) {
+                i++;
+                songs.put(i, track);
+            }
+            StringBuilder sb = new StringBuilder();
+            ArrayList<String> desc = new ArrayList<>();
+            for (String s : args) {
+                if (songs.get(Integer.valueOf(s)) != null) {
+                    manager.getPlayer(event.getGuild()).getListener().tracks.remove(songs.get(Integer.valueOf(s)));
+                    sb.append("**#").append(s).append("**").append(" - ").append(songs.get(Integer.valueOf(s)).getInfo().title).append("\n");
+                    desc.add("**#" + s + "**" + " - " + songs.get(Integer.valueOf(s)).getInfo().title + "\n");
+                }
+                if (s.contains("-")) {
+                    if (s.replaceAll("-", "").matches("0-" + manager.getPlayer(event.getGuild()).getListener().getTrackSize())) {
+                        String[] list = s.split("-");
+                        if (list.length == 2) {
+                            ArrayList<String> numbersBetween = getNumbersBetween(event.getGuild(), Integer.valueOf(list[0]), Integer.valueOf(list[1]));
+                            for (String sl : numbersBetween) {
+                                if (songs.get(Integer.valueOf(sl)) != null) {
+                                    manager.getPlayer(event.getGuild()).getListener().tracks.remove(songs.get(Integer.valueOf(sl)));
+                                    sb.append("**#").append(sl).append("**").append(" - ").append(songs.get(Integer.valueOf(sl)).getInfo().title).append("\n");
+                                    desc.add("**#" + sl + "**" + " - " + songs.get(Integer.valueOf(sl)).getInfo().title + "\n");
+                                }
+                            }
+                        } else {
+                            event.reply("Wrong format!");
+                        }
+                    }
+                }
+            }
+            if (sb.toString().length() > 1900) {
+                sb = new StringBuilder();
+                int pi = 1;
+                for (String s : desc) {
+                    if (sb.toString().length() < 1850) {
+                        sb.append(s);
+                    } else {
+                        EmbedBuilder eb = new EmbedBuilder();
+                        eb.setTitle("Removed part **#" + pi + "**");
+                        eb.setColor(Helpers.EmbedColor);
+                        eb.setDescription(sb.toString());
+                        eb.setFooter(Helpers.getFooterStamp(), Helpers.getFooterIcon());
+                        event.reply(eb.build());
+                        sb = new StringBuilder();
+                        pi++;
+                    }
+                }
+                if (sb.toString().length() != 0) {
+                    EmbedBuilder eb = new EmbedBuilder();
+                    eb.setTitle("Removed part **#" + pi + "**");
+                    eb.setColor(Helpers.EmbedColor);
+                    eb.setDescription(sb.toString());
+                    eb.setFooter(Helpers.getFooterStamp(), Helpers.getFooterIcon());
+                    event.reply(eb.build());
+                }
+            } else {
+                EmbedBuilder eb = new EmbedBuilder();
+                eb.setTitle("Removed");
+                eb.setColor(Helpers.EmbedColor);
+                eb.setDescription(sb.toString());
+                eb.setFooter(Helpers.getFooterStamp(), Helpers.getFooterIcon());
+                event.reply(eb.build());
+            }
+        }
+    }
+
+    private ArrayList<String> getNumbersBetween(Guild guild, int a, int b) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        if (a > 0 && a < manager.getPlayer(guild).getListener().getTrackSize() && b > 0 && b < manager.getPlayer(guild).getListener().getTrackSize()) {
+            if (a < b) {
+                int c = a;
+                while (c <= b) {
+                    toReturn.add(String.valueOf(c));
+                    c++;
+                }
+            } else if (a > b) {
+                int c = a;
+                int d = b;
+                while (c >= d) {
+                    toReturn.add(String.valueOf(c));
+                    d++;
+                }
+            } else if (a == b) {
+                toReturn.add(String.valueOf(a));
+            }
+        }
+        return toReturn;
+    }
+}
