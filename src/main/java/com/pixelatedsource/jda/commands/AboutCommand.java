00ff3d3e54389bc3276ce37c61954938b9ffa704
Merlijn Verstraete <merlijn.verstraete@gmail.com>
2018-01-06 11:47:12 +0100

    Added a section to view the amount of connected voice channels

--- src/main/java/com/pixelatedsource/jda/commands/AboutCommand.java
+++ src/main/java/com/pixelatedsource/jda/commands/AboutCommand.java
@@ -5,6 +5,7 @@ import com.jagrosh.jdautilities.commandclient.CommandEvent;
 import com.pixelatedsource.jda.Helpers;
 import com.pixelatedsource.jda.PixelatedBot;
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Guild;
 
 public class AboutCommand extends Command {
 
@@ -26,6 +27,10 @@ public class AboutCommand extends Command {
         eb.addField("Total user count", String.valueOf(event.getJDA().getUsers().size()), true);
         eb.addField("Libs", "[JDA](https://github.com/DV8FromTheWorld/JDA), [JDA-Utilities](https://github.com/JDA-Applications/JDA-Utilities), [LavaPlayer](https://github.com/sedmelluq/lavaplayer)", false);
         eb.addField("Online time", Helpers.getOnlineTime(), false);
-        event.reply(eb.build());
+        int i = 0;
+        for (Guild guild : event.getJDA().getGuilds()) {
+            if (guild.getAudioManager().isConnected() || guild.getAudioManager().isAttemptingToConnect()) i++;
+        }
+        eb.addField("Playing music count", String.valueOf(i), false); event.reply(eb.build());
     }
 }
