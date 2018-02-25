bd1c891a8e077ce98d99759849a9f862bbbc059b
Merlijn Verstraete <merlijn.verstraete@gmail.com>
2018-02-25 17:09:33 +0100

    added support for @role in setmuterolecommand and fixed a typing error also updated to jda 340

--- src/main/java/com/pixelatedsource/jda/commands/management/SetMuteRoleCommand.java
+++ src/main/java/com/pixelatedsource/jda/commands/management/SetMuteRoleCommand.java
@@ -2,7 +2,11 @@ package com.pixelatedsource.jda.commands.management;
 
 import com.pixelatedsource.jda.Helpers;
 import com.pixelatedsource.jda.PixelSniper;
-import com.pixelatedsource.jda.blub.*;
+import com.pixelatedsource.jda.blub.Category;
+import com.pixelatedsource.jda.blub.Command;
+import com.pixelatedsource.jda.blub.CommandEvent;
+import com.pixelatedsource.jda.blub.RoleType;
+import net.dv8tion.jda.core.entities.Role;
 
 import static com.pixelatedsource.jda.PixelSniper.PREFIX;
 
@@ -12,6 +16,7 @@ public class SetMuteRoleCommand extends Command {
         this.commandName = "setmuterole";
         this.description = "Set the role that will be added to the user when he/she gets muted";
         this.usage = PREFIX + commandName + " <@role | roleId>";
+        this.aliases = new String[] {"smr"};
         this.category = Category.MANAGEMENT;
     }
 
@@ -24,14 +29,20 @@ public class SetMuteRoleCommand extends Command {
                 if (args.length == 0 || args[0].equalsIgnoreCase("")) {
                     if (event.getGuild().getRoleById(role) != null) event.reply(event.getGuild().getRoleById(role).getName());
                     else event.reply("null");
-                } else if (args[0].matches("\\d+")) {
-                    if (PixelSniper.mySQL.setRole(event.getGuild(), args[0], RoleType.MUTE)) {
-                        event.reply("Role channel changed from " + role + " to " + PixelSniper.mySQL.getRoleId(event.getGuild(), RoleType.MUTE));
-                    } else {
-                        event.reply("Failed to set role.");
-                    }
                 } else {
-                    event.reply(usage.replaceFirst(">", PixelSniper.mySQL.getPrefix(event.getGuild().getId())));
+                    Role mutedRole = null;
+                    if (args[0].matches("\\d+")) mutedRole = event.getGuild().getRoleById(args[0]);
+                    else if (event.getMessage().getMentionedRoles().size() > 0) mutedRole = event.getMessage().getMentionedRoles().get(0);
+
+                    if (mutedRole != null) {
+                        if (PixelSniper.mySQL.setRole(event.getGuild(), args[0], RoleType.MUTE)) {
+                            event.reply("MuteRoleId changed from " + role + " to " + PixelSniper.mySQL.getRoleId(event.getGuild(), RoleType.MUTE));
+                        } else {
+                            event.reply("Failed to set role.");
+                        }
+                    } else {
+                        event.reply(usage.replaceFirst(">", PixelSniper.mySQL.getPrefix(event.getGuild().getId())));
+                    }
                 }
             } else {
                 event.reply("You need the permission `" + commandName + "` to execute this command.");
