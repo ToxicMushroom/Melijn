93024641530e38ad3f443e6afc6fd686c490aaff
Merlijn Verstraete <merlijn.verstraete@gmail.com>
2018-09-18 16:40:34 +0200

    Simplified

--- src/main/java/me/melijn/jda/commands/management/PurgeCommand.java
+++ src/main/java/me/melijn/jda/commands/management/PurgeCommand.java
@@ -4,14 +4,9 @@ import me.melijn.jda.Helpers;
 import me.melijn.jda.blub.Category;
 import me.melijn.jda.blub.Command;
 import me.melijn.jda.blub.CommandEvent;
-import me.melijn.jda.utils.Collector;
 import me.melijn.jda.utils.MessageHelper;
 import net.dv8tion.jda.core.Permission;
-import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.MessageHistory;
 
-import java.time.OffsetDateTime;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static me.melijn.jda.Melijn.PREFIX;
@@ -35,31 +30,13 @@ public class PurgeCommand extends Command {
                 String[] args = event.getArgs().split("\\s+");
                 if (args.length == 1 && args[0].matches("^([1-9][0-9]{0,2}|1000)$")) {
                     int toPurgeAmount = Integer.parseInt(args[0]);
-                    MessageHistory messageHistory = event.getTextChannel().getHistory();
-                    Collector<Message> collector = new Collector<>();
-                    while (toPurgeAmount > 0) {
-                        if (toPurgeAmount > 99) {
-                            messageHistory.retrievePast(100).queue(collector::accept);
-                            collector.increment();
-                            toPurgeAmount -= 100;
-                        } else {
-                            messageHistory.retrievePast(toPurgeAmount + 1).queue(collector::accept);
-                            collector.increment();
-                            toPurgeAmount = 0;
-                        }
-                    }
-                    collector.collect(collection -> {
-                        int old = areNotDeletable(collection);
-                        if (old > 0) {
-                            event.reply("Max purge size of this chat is **" + (collection.size() - old) + "** because I can't bulk delete messages older then 2 weeks");
-                        } else {
-                            collection.forEach(message -> MessageHelper.purgedMessageDeleter.putIfAbsent(message.getIdLong(), event.getAuthorId()));
-                            event.getTextChannel().purgeMessages(collection);
-                            event.getTextChannel().sendMessage("**Done**").queue(m -> {
-                                m.delete().queueAfter(3, TimeUnit.SECONDS);
-                                MessageHelper.botDeletedMessages.add(m.getIdLong());
-                            });
-                        }
+                    event.getTextChannel().getIterableHistory().takeAsync(toPurgeAmount + 1).thenAccept(messages -> {
+                        messages.forEach(message -> MessageHelper.purgedMessageDeleter.putIfAbsent(message.getIdLong(), event.getAuthorId()));
+                        event.getTextChannel().purgeMessages(messages);
+                        event.getTextChannel().sendMessage("**Done**").queue(m -> {
+                            m.delete().queueAfter(3, TimeUnit.SECONDS);
+                            MessageHelper.botDeletedMessages.add(m.getIdLong());
+                        });
                     });
                 } else {
                     MessageHelper.sendUsage(this, event);
@@ -70,17 +47,5 @@ public class PurgeCommand extends Command {
         } else {
             event.reply(Helpers.guildOnly);
         }
-    }
-
-    private int areNotDeletable(List<Message> toDelete) {
-        int foundOldMessage = 0;
-        if (toDelete.size() == 0 || OffsetDateTime.now().toEpochSecond() - toDelete.get(toDelete.size() - 1).getCreationTime().toEpochSecond() < 336 * 3600)
-            return foundOldMessage;
-        for (Message msg : toDelete) {
-            if (OffsetDateTime.now().toEpochSecond() - msg.getCreationTime().toEpochSecond() > 336 * 3600) {
-                foundOldMessage++;
-            }
-        }
-        return foundOldMessage;
     }
 }
