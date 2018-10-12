d43e62e471439fed08e3fed77df12d61450e2d55
Merlijn Verstraete <merlijn.verstraete@gmail.com>
2018-10-12 07:37:37 +0200

    Added remove

--- src/main/java/me/melijn/jda/commands/management/HistoryCommand.java
+++ src/main/java/me/melijn/jda/commands/management/HistoryCommand.java
@@ -8,6 +8,8 @@ import me.melijn.jda.blub.CommandEvent;
 import me.melijn.jda.utils.MessageHelper;
 import me.melijn.jda.utils.TaskScheduler;
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.User;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -30,93 +32,98 @@ public class HistoryCommand extends Command {
         if (event.getGuild() != null) {
             if (Helpers.hasPerm(event.getMember(), commandName, 1)) {
                 String[] args = event.getArgs().split("\\s+");
-                if (args.length == 2) {
-                    Helpers.retrieveUserByArgsN(event, args[1], (success) -> {
-                        if (success != null) {
-                            switch (args[0]) {
-                                case "ban":
-                                case "bans":
-                                    TaskScheduler.async(() -> Melijn.mySQL.getUserBans(event.getGuild().getIdLong(), success.getIdLong(), event.getJDA(), bans -> {
-                                        EmbedBuilder ebBan = new EmbedBuilder();
-                                        getLongMessageInParts(new ArrayList<>(Arrays.asList(bans)), parts -> {
-                                                    int partnumber = 0;
-                                                    int size = Integer.parseInt(parts.get(parts.size() - 1));
-                                                    parts.remove(parts.size() - 1);
-                                                    for (String part : parts) {
-                                                        ebBan.setAuthor(success.getName() + "#" + success.getDiscriminator() + "'s bans " + ++partnumber + "/" + size + spaces.substring(0, 45 - success.getName().length()) + "\u200B", null, success.getEffectiveAvatarUrl());
-                                                        ebBan.setDescription(part);
-                                                        ebBan.setColor(Helpers.EmbedColor);
-                                                        event.reply(ebBan.build());
-                                                    }
-                                                }
-                                        );
-                                    }));
-                                    break;
-                                case "mute":
-                                case "mutes":
-                                    TaskScheduler.async(() -> Melijn.mySQL.getUserMutes(event.getGuild().getIdLong(), success.getIdLong(), event.getJDA(), mutes -> {
-                                        EmbedBuilder ebMute = new EmbedBuilder();
-                                        getLongMessageInParts(new ArrayList<>(Arrays.asList(mutes)), parts -> {
-                                                    int partnumber = 0;
-                                                    int size = Integer.parseInt(parts.get(parts.size() - 1));
-                                                    parts.remove(parts.size() - 1);
-                                                    for (String part : parts) {
-                                                        ebMute.setAuthor(success.getName() + "#" + success.getDiscriminator() + "'s mutes " + ++partnumber + "/" + size + spaces.substring(0, 45 - success.getName().length()) + "\u200B", null, success.getEffectiveAvatarUrl());
-                                                        ebMute.setDescription(part);
-                                                        ebMute.setColor(Helpers.EmbedColor);
-                                                        event.reply(ebMute.build());
-                                                    }
-                                                }
-                                        );
-                                    }));
-                                    break;
-                                case "warn":
-                                case "warns":
-                                    TaskScheduler.async(() -> Melijn.mySQL.getUserWarns(event.getGuild().getIdLong(), success.getIdLong(), event.getJDA(), warns -> {
-                                        EmbedBuilder ebWarn = new EmbedBuilder();
-                                        getLongMessageInParts(new ArrayList<>(Arrays.asList(warns)), parts -> {
-                                                    int partnumber = 0;
-                                                    int size = Integer.parseInt(parts.get(parts.size() - 1));
-                                                    parts.remove(parts.size() - 1);
-                                                    for (String part : parts) {
-                                                        ebWarn.setAuthor(success.getName() + "#" + success.getDiscriminator() + "'s warns " + ++partnumber + "/" + size + spaces.substring(0, 45 - success.getName().length()) + "\u200B", null, success.getEffectiveAvatarUrl());
-                                                        ebWarn.setDescription(part);
-                                                        ebWarn.setColor(Helpers.EmbedColor);
-                                                        event.reply(ebWarn.build());
-                                                    }
-                                                }
-                                        );
-                                    }));
-                                    break;
-                                case "kick":
-                                case "kicks":
-                                    TaskScheduler.async(() -> Melijn.mySQL.getUserKicks(event.getGuild().getIdLong(), success.getIdLong(), event.getJDA(), kicks -> {
-                                        EmbedBuilder ebKick = new EmbedBuilder();
-                                        getLongMessageInParts(new ArrayList<>(Arrays.asList(kicks)), parts -> {
-                                                    int partnumber = 0;
-                                                    int size = Integer.parseInt(parts.get(parts.size() - 1));
-                                                    parts.remove(parts.size() - 1);
-                                                    for (String part : parts) {
-                                                        ebKick.setAuthor(success.getName() + "#" + success.getDiscriminator() + "'s kicks " + ++partnumber + "/" + size + spaces.substring(0, 45 - success.getName().length()) + "\u200B", null, success.getEffectiveAvatarUrl());
-                                                        ebKick.setDescription(part);
-                                                        ebKick.setColor(Helpers.EmbedColor);
-                                                        event.reply(ebKick.build());
-                                                    }
-                                                }
-                                        );
-                                    }));
-                                    break;
-                                default:
-                                    MessageHelper.sendUsage(this, event);
-                                    break;
-                            }
-                        } else {
-                            event.reply("Unknown user");
-                        }
-                    });
-                } else {
+                if (args.length < 2) {
                     MessageHelper.sendUsage(this, event);
+                    return;
                 }
+                if (args[0].equalsIgnoreCase("remove")) {
+                    removeSection(event);
+                    return;
+                }
+
+                Helpers.retrieveUserByArgsN(event, args[1], (success) -> {
+                    if (success == null) {
+                        event.reply("Unknown user");
+                        return;
+                    }
+                    switch (args[0]) {
+                        case "ban":
+                        case "bans":
+                            TaskScheduler.async(() -> Melijn.mySQL.getUserBans(event.getGuild().getIdLong(), success.getIdLong(), event.getJDA(), bans -> {
+                                EmbedBuilder ebBan = new EmbedBuilder();
+                                getLongMessageInParts(new ArrayList<>(Arrays.asList(bans)), parts -> {
+                                        int partnumber = 0;
+                                        int size = Integer.parseInt(parts.get(parts.size() - 1));
+                                        parts.remove(parts.size() - 1);
+                                        for (String part : parts) {
+                                            ebBan.setAuthor(success.getName() + "#" + success.getDiscriminator() + "'s bans " + ++partnumber + "/" + size + spaces.substring(0, 45 - success.getName().length()) + "\u200B", null, success.getEffectiveAvatarUrl());
+                                            ebBan.setDescription(part);
+                                            ebBan.setColor(Helpers.EmbedColor);
+                                            event.reply(ebBan.build());
+                                        }
+                                    }
+                                );
+                            }));
+                            break;
+                        case "mute":
+                        case "mutes":
+                            TaskScheduler.async(() -> Melijn.mySQL.getUserMutes(event.getGuild().getIdLong(), success.getIdLong(), event.getJDA(), mutes -> {
+                                EmbedBuilder ebMute = new EmbedBuilder();
+                                getLongMessageInParts(new ArrayList<>(Arrays.asList(mutes)), parts -> {
+                                        int partnumber = 0;
+                                        int size = Integer.parseInt(parts.get(parts.size() - 1));
+                                        parts.remove(parts.size() - 1);
+                                        for (String part : parts) {
+                                            ebMute.setAuthor(success.getName() + "#" + success.getDiscriminator() + "'s mutes " + ++partnumber + "/" + size + spaces.substring(0, 45 - success.getName().length()) + "\u200B", null, success.getEffectiveAvatarUrl());
+                                            ebMute.setDescription(part);
+                                            ebMute.setColor(Helpers.EmbedColor);
+                                            event.reply(ebMute.build());
+                                        }
+                                    }
+                                );
+                            }));
+                            break;
+                        case "warn":
+                        case "warns":
+                            TaskScheduler.async(() -> Melijn.mySQL.getUserWarns(event.getGuild().getIdLong(), success.getIdLong(), event.getJDA(), warns -> {
+                                EmbedBuilder ebWarn = new EmbedBuilder();
+                                getLongMessageInParts(new ArrayList<>(Arrays.asList(warns)), parts -> {
+                                        int partnumber = 0;
+                                        int size = Integer.parseInt(parts.get(parts.size() - 1));
+                                        parts.remove(parts.size() - 1);
+                                        for (String part : parts) {
+                                            ebWarn.setAuthor(success.getName() + "#" + success.getDiscriminator() + "'s warns " + ++partnumber + "/" + size + spaces.substring(0, 45 - success.getName().length()) + "\u200B", null, success.getEffectiveAvatarUrl());
+                                            ebWarn.setDescription(part);
+                                            ebWarn.setColor(Helpers.EmbedColor);
+                                            event.reply(ebWarn.build());
+                                        }
+                                    }
+                                );
+                            }));
+                            break;
+                        case "kick":
+                        case "kicks":
+                            TaskScheduler.async(() -> Melijn.mySQL.getUserKicks(event.getGuild().getIdLong(), success.getIdLong(), event.getJDA(), kicks -> {
+                                EmbedBuilder ebKick = new EmbedBuilder();
+                                getLongMessageInParts(new ArrayList<>(Arrays.asList(kicks)), parts -> {
+                                        int partnumber = 0;
+                                        int size = Integer.parseInt(parts.get(parts.size() - 1));
+                                        parts.remove(parts.size() - 1);
+                                        for (String part : parts) {
+                                            ebKick.setAuthor(success.getName() + "#" + success.getDiscriminator() + "'s kicks " + ++partnumber + "/" + size + spaces.substring(0, 45 - success.getName().length()) + "\u200B", null, success.getEffectiveAvatarUrl());
+                                            ebKick.setDescription(part);
+                                            ebKick.setColor(Helpers.EmbedColor);
+                                            event.reply(ebKick.build());
+                                        }
+                                    }
+                                );
+                            }));
+                            break;
+                        default:
+                            MessageHelper.sendUsage(this, event);
+                            break;
+                    }
+                });
             } else {
                 event.reply("You need the permission `" + commandName + "` to execute this command.");
             }
@@ -144,5 +151,50 @@ public class HistoryCommand extends Command {
         if (part.length() > 0) parts.add(part.toString());
         parts.add(String.valueOf(count));
         callback.accept(parts);
+    }
+
+    private void removeSection(CommandEvent event) {
+        String[] args = event.getArgs().split("\\s+");
+        Guild guild = event.getGuild();
+        if (args.length < 4) {
+            event.reply("Usage: " + SetPrefixCommand.prefixes.getUnchecked(guild.getIdLong()) + commandName + " remove <ban | warn | kick | mute> <user> <moment>");
+            return;
+        }
+
+        User user = Helpers.getUserByArgs(event, args[2]);
+        if (user == null || guild.getMember(user) == null) {
+            event.reply("Unknown member");
+            return;
+        }
+
+        long millis = MessageHelper.dateToMillis(event.getArgs().replaceFirst("\\s+" + args[0] + "\\s+" + args[1] + "\\s+", ""));
+
+        if (millis == -1) {
+            event.reply("Wrong time format it should be: `h:m:s`s `d:M:yyyy`");
+            return;
+        }
+
+        Melijn.channelLog(String.valueOf(millis));
+        switch (args[1]) {
+            case "ban":
+                Melijn.mySQL.removeBan(guild.getMember(user), millis);
+                event.reply("Successfully removed the ban");
+                break;
+            case "mute":
+                Melijn.mySQL.removeMute(guild.getMember(user), millis);
+                event.reply("Successfully removed the mute");
+                break;
+            case "warn":
+                Melijn.mySQL.removeWarn(guild.getMember(user), millis);
+                event.reply("Successfully removed the warn");
+                break;
+            case "kick":
+                Melijn.mySQL.removeKick(guild.getMember(user), millis);
+                event.reply("Successfully removed the kick");
+                break;
+            default:
+                event.reply("Usage: " + SetPrefixCommand.prefixes.getUnchecked(guild.getIdLong()) + commandName + " remove <ban | warn | kick | mute> <user> <moment>");
+                break;
+        }
     }
 }
