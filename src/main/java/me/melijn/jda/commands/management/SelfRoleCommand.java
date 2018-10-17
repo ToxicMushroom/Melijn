0bec088fe313a35e9d1c3b944b1579793fe1e636
Merlijn Verstraete <merlijn.verstraete@gmail.com>
2018-10-17 21:11:43 +0200

    Added a feature

---
+++ src/main/java/me/melijn/jda/commands/management/SelfRoleCommand.java
@@ -0,0 +1,92 @@
+package me.melijn.jda.commands.management;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import gnu.trove.map.TLongObjectMap;
+import me.melijn.jda.Helpers;
+import me.melijn.jda.Melijn;
+import me.melijn.jda.blub.Category;
+import me.melijn.jda.blub.Command;
+import me.melijn.jda.blub.CommandEvent;
+import me.melijn.jda.blub.Need;
+import me.melijn.jda.utils.MessageHelper;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Role;
+import net.dv8tion.jda.core.utils.cache.SnowflakeCacheView;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.TimeUnit;
+
+import static me.melijn.jda.Melijn.PREFIX;
+
+public class SelfRoleCommand extends Command {
+
+    public static final LoadingCache<Long, TLongObjectMap<String>> selfRoles = CacheBuilder.newBuilder()
+            .maximumSize(30)
+            .expireAfterAccess(1, TimeUnit.MINUTES)
+            .build(new CacheLoader<>() {
+                public TLongObjectMap<String> load(@NotNull Long key) {
+                    return Melijn.mySQL.getSelfRoles(key);
+                }
+            });
+
+    public SelfRoleCommand() {
+        this.commandName = "selfRole";
+        this.description = "Ma in command to add and remove SelfRoles";
+        this.usage = PREFIX + commandName + " <add | remove | list> [role] [emote | emoji]";
+        this.category = Category.MANAGEMENT;
+        this.needs = new Need[]{Need.GUILD};
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        if (Helpers.hasPerm(event.getMember(), commandName, 1)) {
+            String[] args = event.getArgs().split("\\s+");
+            Guild guild = event.getGuild();
+            if (args.length == 0) {
+                MessageHelper.sendUsage(this, event);
+                return;
+            }
+            switch (args[0].toLowerCase()) {
+                case "add":
+                    if (args.length < 2 && event.getMessage().getEmotes().size() > 0 || args[2].matches("\\\\u....")) {
+                        event.reply(SetPrefixCommand.prefixes.getUnchecked(guild.getIdLong()) + commandName + " add <role> <emote | emoji>");
+                        return;
+                    }
+
+                    String emote = event.getMessage().getEmotes().size() > 0 ? event.getMessage().getEmotes().get(0).getId() : args[2];
+                    Role roleAdded = Helpers.getRoleByArgs(event, args[1]);
+                    if (roleAdded != null && roleAdded.getIdLong() != guild.getIdLong()) {
+                        Melijn.mySQL.addSelfRole(guild.getIdLong(), roleAdded.getIdLong(), emote);
+                        event.reply("SelfRole added: **@" + roleAdded.getName() + "** by **" + event.getFullAuthorName() + "**");
+                    } else {
+                        event.reply(SetPrefixCommand.prefixes.getUnchecked(guild.getIdLong()) + commandName + " add <role> <emote | emoji>");
+                    }
+                    break;
+                case "remove":
+                    Role roleRemoved = Helpers.getRoleByArgs(event, args[1]);
+                    if (roleRemoved != null && roleRemoved.getIdLong() != guild.getIdLong()) {
+                        Melijn.mySQL.removeSelfRole(guild.getIdLong(), roleRemoved.getIdLong());
+                        event.reply("SelfRole removed: **@" + roleRemoved.getName() + "** by **" + event.getFullAuthorName() + "**");
+                    } else {
+                        event.reply(SetPrefixCommand.prefixes.getUnchecked(guild.getIdLong()) + commandName + " add <role>");
+                    }
+                    break;
+                case "list":
+                    StringBuilder sb = new StringBuilder("**SelfRoles**\n```INI");
+                    TLongObjectMap<String> rolesIds = selfRoles.getUnchecked(guild.getIdLong());
+                    for (int i = 0; i < rolesIds.keys().length; i++) {
+                        SnowflakeCacheView<Role> roles = guild.getRoleCache();
+                        Role role = roles.getElementById(rolesIds.keys()[i]);
+                        if (role != null)
+                            sb.append("\n").append(i+1).append(" - [").append(role.getName()).append("] - ").append(rolesIds.get(rolesIds.keys()[i]));
+                    }
+                    sb.append("```");
+                    if (rolesIds.keys().length == 0) sb.append("There are no SelfRoles");
+                    MessageHelper.sendSplitMessage(event.getTextChannel(), sb.toString());
+                    break;
+            }
+        }
+    }
+}
---
+++ src/main/java/me/melijn/jda/commands/management/SetSelfRoleChannelCommand.java
@@ -0,0 +1,57 @@
+package me.melijn.jda.commands.management;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import me.melijn.jda.Helpers;
+import me.melijn.jda.Melijn;
+import me.melijn.jda.blub.Category;
+import me.melijn.jda.blub.Command;
+import me.melijn.jda.blub.CommandEvent;
+import me.melijn.jda.blub.Need;
+import me.melijn.jda.utils.MessageHelper;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.TimeUnit;
+
+import static me.melijn.jda.Melijn.PREFIX;
+import static me.melijn.jda.blub.ChannelType.SELF_ROLE;
+
+public class SetSelfRoleChannelCommand extends Command {
+
+    public static final LoadingCache<Long, Long> selfRolesChannel = CacheBuilder.newBuilder()
+            .maximumSize(30)
+            .expireAfterAccess(5, TimeUnit.MINUTES)
+            .build(new CacheLoader<>() {
+                public Long load(@NotNull Long key) {
+                    return Melijn.mySQL.getChannelId(key, SELF_ROLE);
+                }
+            });
+
+    public SetSelfRoleChannelCommand() {
+        this.commandName = "setSelfRoleChannel";
+        this.usage = PREFIX + commandName + " [TextChannel]";
+        this.description = "Set or view the selfRoleChannel";
+        this.needs = new Need[]{Need.GUILD};
+        this.category = Category.MANAGEMENT;
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        if (Helpers.hasPerm(event.getMember(), commandName, 1)) {
+            String[] args = event.getArgs().split("\\s+");
+            if (args.length == 0 || args[0].isBlank()) {
+                String s = selfRolesChannel.getUnchecked(event.getGuild().getIdLong()) == -1 ? "nothing" : "<#" + selfRolesChannel.getUnchecked(event.getGuild().getIdLong()) + ">";
+                event.reply("SelfRoleChannel: " + s);
+            } else {
+                long channel = Helpers.getTextChannelByArgsN(event, args[0]);
+                if (channel != -1) {
+                    Melijn.mySQL.setChannel(event.getGuild().getIdLong(), channel, SELF_ROLE);
+                    event.reply("The SelfRoleChannel has been changed to <#" + channel + "> by **" + event.getFullAuthorName() + "**");
+                } else {
+                    MessageHelper.sendUsage(this, event);
+                }
+            }
+        }
+    }
+}
