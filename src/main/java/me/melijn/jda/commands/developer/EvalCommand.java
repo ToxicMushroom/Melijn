ce857d2860c148d4b0a7272dd4b0f5f5b82d380c
Merlijn Verstraete <merlijn.verstraete@gmail.com>
2018-10-20 09:05:29 +0200

    new eval style

--- src/main/java/me/melijn/jda/commands/developer/EvalCommand.java
+++ src/main/java/me/melijn/jda/commands/developer/EvalCommand.java
@@ -2,28 +2,23 @@ package me.melijn.jda.commands.developer;
 
 import gnu.trove.list.TLongList;
 import gnu.trove.list.array.TLongArrayList;
-import me.melijn.jda.Helpers;
-import me.melijn.jda.Melijn;
 import me.melijn.jda.blub.Category;
 import me.melijn.jda.blub.Command;
 import me.melijn.jda.blub.CommandEvent;
 import me.melijn.jda.utils.MessageHelper;
 import me.melijn.jda.utils.TaskScheduler;
-import me.melijn.jda.utils.WebUtils;
-import net.dv8tion.jda.core.EmbedBuilder;
-import org.codehaus.groovy.jsr223.GroovyScriptEngineImpl;
+import org.codehaus.commons.compiler.CompilerFactoryFactory;
+import org.codehaus.commons.compiler.ISimpleCompiler;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
+import java.lang.reflect.Method;
 
 import static me.melijn.jda.Melijn.PREFIX;
 
 public class EvalCommand extends Command {
 
-    public static EvalCommand INSTANCE = new EvalCommand();
     public static TLongList serverBlackList = new TLongArrayList();
     public static TLongList userBlackList = new TLongArrayList();
-    String engineName = "groovy";
+    private static final String CLASS_NAME = "EvalTempClass";
 
     public EvalCommand() {
         this.commandName = "eval";
@@ -34,55 +29,52 @@ public class EvalCommand extends Command {
 
     @Override
     protected void execute(CommandEvent event) {
-        if (engineName.equalsIgnoreCase("groovy")) {
-            GroovyScriptEngineImpl se = (GroovyScriptEngineImpl) new ScriptEngineManager().getEngineByName("groovy");
-            evaluate(event, se);
-        } else {
-            ScriptEngine se = new ScriptEngineManager().getEngineByName(engineName);
-            evaluate(event, se);
-        }
-    }
-
-    private void evaluate(CommandEvent event, ScriptEngine se) {
-        if (se == null) {
-            MessageHelper.sendUsage(this, event);
-            return;
-        }
-
-        String imports = "import java.io.*;\n" +
-                "import java.lang.*;\n" +
-                "import java.util.*;\n" +
-                "import net.dv8tion.jda.core.*;\n" +
-                "import me.melijn.jda.blub.*;\n" +
-                "import me.melijn.jda.commands.developer.*;\n" +
-                "import me.melijn.jda.commands.management.*;\n" +
-                "import me.melijn.jda.commands.music.*;\n" +
-                "import me.melijn.jda.commands.util.*;\n" +
-                "import me.melijn.jda.Melijn;\n" +
-                "import me.melijn.jda.Helpers;\n";
-
-
         TaskScheduler.async(() -> {
-            se.put("event", event);
-            se.put("jda", event.getJDA());
-            se.put("guild", event.getGuild());
-            se.put("channel", event.getChannel());
-            se.put("message", event.getMessage());
-            se.put("mysql", Melijn.mySQL);
-            se.put("eb", new EmbedBuilder());
-            se.put("webUtils", WebUtils.getWebUtilsInstance());
-            se.put("serverBlackList", serverBlackList);
-            se.put("userBlackList", userBlackList);
-            se.put("voteReq", Helpers.voteChecks);
-
             try {
-                if (event.getArgs().contains("event.reply("))
-                    se.eval(imports + event.getArgs());
-                else
-                    event.reply(se.eval(imports + event.getArgs()).toString());
+                evaluate(event.getArgs(), event);
             } catch (Exception e) {
-                event.reply("An exception was thrown:\n```\n" + e + "```");
+                MessageHelper.sendSplitMessage(event.getTextChannel(), "```" + e.getMessage() + "```");
             }
         });
+    }
+
+    private static void evaluate(final String source, CommandEvent event) throws Exception {
+        final ISimpleCompiler compiler = CompilerFactoryFactory.getDefaultCompilerFactory().newSimpleCompiler();
+        compiler.cook(createDummyClassSource(source));
+        evaluateDummyClassMethod(event, compiler.getClassLoader());
+    }
+
+    private static String createDummyClassSource(final String source) {
+        return  "import me.melijn.jda.blub.*;\n" +
+                "import me.melijn.jda.utils.*;\n" +
+                "import me.melijn.jda.Melijn;\n" +
+                "import me.melijn.jda.commands.*;\n" +
+                "import me.melijn.jda.commands.management.*;\n" +
+                "import me.melijn.jda.commands.util.*;\n" +
+                "import me.melijn.jda.commands.music.*;\n" +
+                "import me.melijn.jda.commands.fun.*;\n" +
+                "import me.melijn.jda.commands.developer.*;\n" +
+                "import java.io.*;\n" +
+                "import java.lang.*;\n" +
+                "import java.util.*;\n" +
+                "import java.util.concurrent.*;\n" +
+                "import net.dv8tion.jda.core.*;\n" +
+                "import net.dv8tion.jda.core.entities.*;\n" +
+                "import net.dv8tion.jda.core.entities.impl.*;\n" +
+                "import net.dv8tion.jda.core.managers.*;\n" +
+                "import net.dv8tion.jda.core.managers.impl.*;\n" +
+                "import net.dv8tion.jda.core.utils.*;\n" +
+                "class " + CLASS_NAME + " {\n" +
+                "   public static void eval(final CommandEvent event) {\n" +
+                "       " + source + "\n" +
+                "   }\n" +
+                "}\n";
+    }
+
+    private static void evaluateDummyClassMethod(final CommandEvent event, final ClassLoader classLoader) throws Exception {
+        final Class<?> dummy = classLoader.loadClass(CLASS_NAME);
+        final Method eval = dummy.getDeclaredMethod("eval", CommandEvent.class);
+        eval.setAccessible(true);
+        eval.invoke(null, event);
     }
 }
