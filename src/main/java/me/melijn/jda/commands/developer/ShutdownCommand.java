e10d924d5beebcfd009b577981cb8134a522a2d6
Merlijn Verstraete <merlijn.verstraete@gmail.com>
2018-10-29 21:00:24 +0100

    Added ShutDownCommand

---
+++ src/main/java/me/melijn/jda/commands/developer/ShutdownCommand.java
@@ -0,0 +1,52 @@
+package me.melijn.jda.commands.developer;
+
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import gnu.trove.map.TLongObjectMap;
+import me.melijn.jda.Helpers;
+import me.melijn.jda.Melijn;
+import me.melijn.jda.blub.Category;
+import me.melijn.jda.blub.Command;
+import me.melijn.jda.blub.CommandEvent;
+import me.melijn.jda.music.MusicManager;
+import me.melijn.jda.music.MusicPlayer;
+import me.melijn.jda.utils.TaskScheduler;
+
+import java.io.File;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static me.melijn.jda.Melijn.PREFIX;
+
+public class ShutdownCommand extends Command {
+
+    public ShutdownCommand() {
+        this.commandName = "shutdown";
+        this.description = "shut's the bot nicely down";
+        this.usage = PREFIX + commandName;
+        this.category = Category.DEVELOPER;
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        File file = new File(Melijn.class.getClassLoader().getResource("Melijn.mp3").getFile());
+
+        //save players before shutdown
+        //Duncte rated: niet mooi
+        TLongObjectMap<MusicPlayer> players = MusicManager.getManagerInstance().getPlayers();
+        players.forEachValue((player) -> {
+            TaskScheduler.async(() -> Helpers.scheduleClose(player.getGuild().getAudioManager()), 9000);
+            boolean paused = player.getPaused();
+            BlockingQueue<AudioTrack> queue = new LinkedBlockingQueue<>();
+            if (player.getAudioPlayer().getPlayingTrack() != null)
+                queue.offer(player.getAudioPlayer().getPlayingTrack());
+            player.getListener().getTracks().forEach(queue::offer);
+            Melijn.mySQL.addQueue(player.getGuild().getIdLong(), player.getGuild().getSelfMember().getVoiceState().getChannel().getIdLong(), paused, queue);
+            player.getAudioPlayer().stopTrack();
+            player.getListener().getTracks().clear();
+            MusicManager.getManagerInstance().loadSimpleTrack(player.getGuild(), file.getPath());
+            return true;
+        });
+
+        TaskScheduler.async(() -> event.getJDA().shutdown(), 9000);
+    }
+}
