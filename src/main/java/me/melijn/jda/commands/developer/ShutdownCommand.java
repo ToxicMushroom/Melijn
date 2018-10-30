6966b796405c28fd60a3c52a01bfc0d8988ddd2f
Merlijn Verstraete <merlijn.verstraete@gmail.com>
2018-10-30 17:01:53 +0100

    Now works on linux #JAY

--- src/main/java/me/melijn/jda/commands/developer/ShutdownCommand.java
+++ src/main/java/me/melijn/jda/commands/developer/ShutdownCommand.java
@@ -10,8 +10,13 @@ import me.melijn.jda.blub.CommandEvent;
 import me.melijn.jda.music.MusicManager;
 import me.melijn.jda.music.MusicPlayer;
 import me.melijn.jda.utils.TaskScheduler;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -28,25 +33,35 @@ public class ShutdownCommand extends Command {
 
     @Override
     protected void execute(CommandEvent event) {
-        File file = new File(Melijn.class.getClassLoader().getResource("Melijn.mp3").getFile());
+        URL resource = getClass().getClassLoader().getResource("Melijn.mp3");
+        if (resource == null) {
+            event.reply("mp3 not found :/");
+            return;
+        }
+        try {
+            File tempFile = File.createTempFile(FilenameUtils.getBaseName(resource.getFile()), FilenameUtils.getExtension(resource.getFile()));
+            IOUtils.copy(resource.openStream(), FileUtils.openOutputStream(tempFile));
 
-        //save players before shutdown
-        //Duncte rated: niet mooi
-        TLongObjectMap<MusicPlayer> players = MusicManager.getManagerInstance().getPlayers();
-        players.forEachValue((player) -> {
-            TaskScheduler.async(() -> Helpers.scheduleClose(player.getGuild().getAudioManager()), 9000);
-            boolean paused = player.getPaused();
-            BlockingQueue<AudioTrack> queue = new LinkedBlockingQueue<>();
-            if (player.getAudioPlayer().getPlayingTrack() != null)
-                queue.offer(player.getAudioPlayer().getPlayingTrack());
-            player.getListener().getTracks().forEach(queue::offer);
-            Melijn.mySQL.addQueue(player.getGuild().getIdLong(), player.getGuild().getSelfMember().getVoiceState().getChannel().getIdLong(), paused, queue);
-            player.getAudioPlayer().stopTrack();
-            player.getListener().getTracks().clear();
-            MusicManager.getManagerInstance().loadSimpleTrack(player.getGuild(), file.getPath());
-            return true;
-        });
-
-        TaskScheduler.async(() -> event.getJDA().shutdown(), 9000);
+            //save players before shutdown
+            TLongObjectMap<MusicPlayer> players = MusicManager.getManagerInstance().getPlayers();
+            players.forEachValue((player) -> {
+                TaskScheduler.async(() -> Helpers.scheduleClose(player.getGuild().getAudioManager()), 9000);
+                boolean paused = player.getPaused();
+                BlockingQueue<AudioTrack> queue = new LinkedBlockingQueue<>();
+                if (player.getAudioPlayer().getPlayingTrack() != null)
+                    queue.offer(player.getAudioPlayer().getPlayingTrack());
+                player.getListener().getTracks().forEach(queue::offer);
+                Melijn.mySQL.addQueue(player.getGuild().getIdLong(), player.getGuild().getSelfMember().getVoiceState().getChannel().getIdLong(), paused, queue);
+                player.getAudioPlayer().stopTrack();
+                player.getListener().getTracks().clear();
+                MusicManager.getManagerInstance().loadSimpleTrack(player.getGuild(), tempFile.getPath());
+                return true;
+            });
+            event.reply("Shutting down in 9 seconds");
+            TaskScheduler.async(() -> event.getJDA().shutdown(), 9000);
+        } catch (IOException e) {
+            e.printStackTrace();
+            event.reply("mp3 not found :/");
+        }
     }
 }
