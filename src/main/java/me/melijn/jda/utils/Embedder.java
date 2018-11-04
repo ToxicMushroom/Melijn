2580c0facea12ca55a630f3b47ccd19df89869bc
Merlijn Verstraete <merlijn.verstraete@gmail.com>
2018-11-04 21:27:15 +0100

    Added embedder for future use

---
+++ src/main/java/me/melijn/jda/utils/Embedder.java
@@ -0,0 +1,12 @@
+package me.melijn.jda.utils;
+
+import me.melijn.jda.commands.management.SetEmbedColorCommand;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Guild;
+
+public class Embedder extends EmbedBuilder {
+
+    public Embedder(Guild guild) {
+        setColor(SetEmbedColorCommand.embedColorCache.getUnchecked(guild.getIdLong()));
+    }
+}
