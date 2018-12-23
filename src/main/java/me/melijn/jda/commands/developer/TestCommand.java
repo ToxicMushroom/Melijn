9b2ae7f181defc4495c56e7407873d8298295aba
Merlijn Verstraete <merlijn.verstraete@gmail.com>
2018-12-23 20:12:47 +0100

    oof

--- src/main/java/me/melijn/jda/commands/developer/TestCommand.java
+++ src/main/java/me/melijn/jda/commands/developer/TestCommand.java
@@ -21,7 +21,25 @@ public class TestCommand extends Command {
     @Override
     protected void execute(CommandEvent event) {
         //event.reply("test command is not in use atm");
+
+
+
         ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) TaskScheduler.EXECUTOR_SERVICE;
         event.reply(threadPoolExecutor.getActiveCount());
+
+/*
+        String[] args = event.getArgs().split("\\s+");
+        if (args[0].matches("100(?:,0)?%?|\\d{1,2}(?:,\\d)?%?")) {
+            float percent = Float.parseFloat(args[0].replaceAll("%", ""));
+            BufferedImage bufferedImage = new BufferedImage(1020, 120, BufferedImage.TYPE_INT_RGB);
+            Graphics2D graphics2D = bufferedImage.createGraphics();
+            graphics2D.setColor(Color.BLACK);
+            graphics2D.fillRect(0,0, bufferedImage.getWidth(), bufferedImage.getHeight());
+            graphics2D.setColor(Color.GRAY);
+            graphics2D.fillRect(10, 10, bufferedImage.getWidth() - 20, bufferedImage.getHeight() - 20);
+            graphics2D.setColor(new Color(SetEmbedColorCommand.embedColorCache.getUnchecked(event.getGuild().getIdLong())));
+            graphics2D.fillRect(10, 10, Math.round(percent * 10), 100);
+            event.reply(bufferedImage);
+        }*/
     }
 }
