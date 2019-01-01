75d8a661afe859787a57685225be89459360be48
Duncan Sterken <duncan.sterken123@gmail.com>
2019-01-01 12:06:35 +0100

    Run youtube in it's own thread

--- src/main/java/me/melijn/jda/utils/YTSearch.java
+++ src/main/java/me/melijn/jda/utils/YTSearch.java
@@ -13,17 +13,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
 public class YTSearch {
 
+    private static YouTube youtube;
+    private static Properties properties;
     //https://github.com/youtube/api-samples/blob/master/java/src/main/java/com/google/api/services/samples/youtube/cmdline/data/Search.java
     private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
     private static final JsonFactory JSON_FACTORY = new JacksonFactory();
-
     private static final String PROPERTIES_FILENAME = "youtube.properties";
-    private static YouTube youtube;
-    private static Properties properties;
+    private final ExecutorService youtubeService = Executors.newCachedThreadPool(
+            (r) -> new Thread(r, "Youtube-Search-Thread")
+    );
 
     public YTSearch() {
         properties = new Properties();
@@ -39,42 +43,45 @@ public class YTSearch {
     }
 
     public void search(String query, Consumer<String> videoCallback) {
-        try {
-            YouTube.Search.List search = youtube.search().list("id");
+        youtubeService.submit(() -> {
+            try {
+                YouTube.Search.List search = youtube.search().list("id");
 
 
-            // Set your developer key from the {{ Google Cloud Console }} for
-            // non-authenticated requests. See:
-            // {{ https://cloud.google.com/console }}
-            String apiKey = properties.getProperty("youtube.apikey");
-            search.setKey(apiKey);
-            search.setQ(query);
+                // Set your developer key from the {{ Google Cloud Console }} for
+                // non-authenticated requests. See:
+                // {{ https://cloud.google.com/console }}
+                String apiKey = properties.getProperty("youtube.apikey");
+                search.setKey(apiKey);
+                search.setQ(query);
 
-            // Restrict the search results to only include videos. See:
-            // https://developers.google.com/youtube/v3/docs/search/list#type
-            search.setType("video");
+                // Restrict the search results to only include videos. See:
+                // https://developers.google.com/youtube/v3/docs/search/list#type
+                search.setType("video");
 
-            // To increase efficiency, only retrieve the fields that the
-            // application uses.
-            search.setFields("items(id/videoId)");
-            search.setMaxResults(1L);
+                // To increase efficiency, only retrieve the fields that the
+                // application uses.
+                search.setFields("items(id/videoId)");
+                search.setMaxResults(1L);
 
-            // Call the API adn return results
-            SearchListResponse searchResponse = search.execute();
-            List<SearchResult> searchResultList = searchResponse.getItems();
-            if (searchResultList.size() == 0) {
-                videoCallback.accept(null);
+                // Call the API adn return results
+                SearchListResponse searchResponse = search.execute();
+                List<SearchResult> searchResultList = searchResponse.getItems();
+                if (searchResultList.size() == 0) {
+                    videoCallback.accept(null);
+                }
+                String id = searchResultList.get(0).getId().getVideoId();
+                videoCallback.accept(id);
+            } catch (GoogleJsonResponseException e) {
+                System.err.println("There was a service error: " + e.getDetails().getCode() + " : " + e.getDetails().getMessage());
+            } catch (IOException e) {
+                System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
+            } catch (Throwable t) {
+                t.printStackTrace();
             }
-            String id =  searchResultList.get(0).getId().getVideoId();
-            videoCallback.accept(id);
-        } catch (GoogleJsonResponseException e) {
-            System.err.println("There was a service error: " + e.getDetails().getCode() + " : " + e.getDetails().getMessage());
-        } catch (IOException e) {
-            System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
-        } catch (Throwable t) {
-            t.printStackTrace();
-        }
 
-        videoCallback.accept(null);
+            videoCallback.accept(null);
+        });
     }
+
 }
