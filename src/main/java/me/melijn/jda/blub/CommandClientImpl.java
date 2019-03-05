39364aea35abe405b4ad89ae9e3198b1d6df159d
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-03-05 14:41:09 +0100

    Last npe fix didn't work

--- src/main/java/me/melijn/jda/blub/CommandClientImpl.java
+++ src/main/java/me/melijn/jda/blub/CommandClientImpl.java
@@ -163,7 +163,11 @@ public class CommandClientImpl extends ListenerAdapter implements CommandClient 
         if (command.getCategory() == Category.DEVELOPER && event.getAuthor().getIdLong() != Melijn.OWNERID)
             return true;
         if (noPermission(event, command)) return true;
-        if (event.getGuild() != null && !event.getMember().hasPermission(Permission.ADMINISTRATOR) && event.getTextChannel().getTopic().contains("\u200B")) return true;
+        if (event.getGuild() != null &&
+                !event.getMember().hasPermission(Permission.ADMINISTRATOR) &&
+                event.getTextChannel() != null &&
+                event.getTextChannel().getTopic() == null &&
+                event.getTextChannel().getTopic().contains("\u200B")) return true;
         return unFulfilledNeeds(event, command);
     }
 
