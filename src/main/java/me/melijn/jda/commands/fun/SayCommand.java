93ce59f993ad6ab00d9bd127c6caddb75c97e24b
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-04-10 17:58:24 +0200

    Updated say command so it works more dynamic and uses the new method in ImageUtils

--- src/main/java/me/melijn/jda/commands/fun/SayCommand.java
+++ src/main/java/me/melijn/jda/commands/fun/SayCommand.java
@@ -3,6 +3,7 @@ package me.melijn.jda.commands.fun;
 import me.melijn.jda.blub.Category;
 import me.melijn.jda.blub.Command;
 import me.melijn.jda.blub.CommandEvent;
+import me.melijn.jda.utils.ImageUtils;
 import net.dv8tion.jda.core.Permission;
 
 import javax.imageio.ImageIO;
@@ -18,7 +19,7 @@ public class SayCommand extends Command {
     public SayCommand() {
         this.commandName = "say";
         this.description = "Makes the bot say stuff";
-        this.usage = PREFIX + commandName + " <message>";
+        this.usage = PREFIX + commandName + " [%fontSize%, default is 60] <text>";
         this.aliases = new String[]{"zeg"};
         this.permissions = new Permission[]{Permission.MESSAGE_ATTACH_FILES};
         this.category = Category.FUN;
@@ -28,56 +29,47 @@ public class SayCommand extends Command {
     @Override
     protected void execute(CommandEvent event) {
         if (event.getGuild() == null || event.hasPerm(event.getMember(), commandName, 0)) {
-            if (event.getArgs().length() > 0) {
-                final BufferedImage image;
-                try {
-                    String resourcename = event.getExecutor().equalsIgnoreCase("zeg") ? "melijn_zegt.png" : "melijn_says.png";
-                    image = ImageIO.read(new File(resourcename));
-                    Graphics g = image.getGraphics();
-                    g.setFont(g.getFont().deriveFont(40f));
-                    if (event.getArgs().length() < 26) {
-                        g.drawString(event.getArgs(), 650, 200);
-                    } else {
-                        StringBuilder sb = new StringBuilder();
-                        String[] parts = event.getArgs().split("\\s+");
-                        for (String part : parts) {
-                            if (part.length() > 25) {
-                                String[] characters = part.split("");
-                                int i = 0;
-                                for (String charl : characters) {
-                                    sb.append(charl);
-                                    if (i++ == 22) {
-                                        sb.append("-\n");
-                                        i = 0;
-                                    }
-                                }
-                            } else if ((sb.toString().split("\n")[sb.toString().split("\n").length - 1].length() + part.length()) > 28) {
-                                sb.append("\n").append(part);
-                            } else {
-                                if (sb.length() > 0) sb.append(" ");
-                                sb.append(part);
-                            }
-                        }
-                        int i = 0;
-                        String[] lines = sb.toString().split("\n");
-                        for (String line : lines) {
-                            g.drawString(line, 640, 220 - (20 * lines.length) + (i++ * 40));
+            if (event.getArgs().isEmpty()) {
+                event.sendUsage(this, event);
+                return;
+            }
+            BufferedImage image;
+            try {
+                String resourcename = event.getExecutor().equalsIgnoreCase("zeg") ? "melijn_zegt.jpg" : "melijn_says.jpg";
+                image = ImageIO.read(new File(resourcename));
+                String[] args = event.getArgs().split("\\s+");
+                Graphics graphics = image.getGraphics();
+                Font font = graphics.getFont().deriveFont(60f);
+                String text = event.getArgs();
 
-                        }
+                if (args[0].matches("%\\d+(?:.\\d+)?%")) {
+                    try {
+                        float fontsize = Float.parseFloat(args[0].substring(1, args[0].length() - 1));
+                        font = graphics.getFont().deriveFont(fontsize);
+                    } catch (NumberFormatException ignored) {
+                        event.reply("The number inside the %% signs must be under 20 figures");
                     }
-                    g.dispose();
-                    String imageName = String.valueOf(System.currentTimeMillis());
-                    File file = new File(imageName + ".png");
-                    ImageIO.write(image, "png", file);
-                    event.getTextChannel().sendFile(file).queue(q -> file.delete());
-
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    text = text.replaceFirst(args[0] + "\\s+?", "");
                 }
 
-            } else {
-                event.sendUsage(this, event);
+
+                graphics.setFont(font);
+                graphics.setColor(Color.DARK_GRAY);
+
+                int startx = 1133;
+                int endx = 1800;
+                int starty = 82;
+                int endy = 1000;
+
+                ImageUtils imageUtils = new ImageUtils();
+                image = imageUtils.putText(image, text, startx, endx, starty, endy, graphics);
+
+                event.reply(image);
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+
         } else {
             event.reply("You need the permission `" + commandName + "` to execute this command.");
         }
