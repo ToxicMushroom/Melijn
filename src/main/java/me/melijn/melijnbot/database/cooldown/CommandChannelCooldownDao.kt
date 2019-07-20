6b29c89f846b8bd12d2f4375c7f3a216205ff064
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-07-20 11:54:36 +0200

    added channel cooldown dao

--- src/main/java/me/melijn/melijnbot/database/cooldown/CommandChannelCooldownDao.kt
+++ src/main/java/me/melijn/melijnbot/database/cooldown/CommandChannelCooldownDao.kt
@@ -1,4 +1,25 @@
 package me.melijn.melijnbot.database.cooldown
 
-class CommandChannelCooldownDao {
+import me.melijn.melijnbot.database.Dao
+import me.melijn.melijnbot.database.DriverManager
+import java.util.function.Consumer
+
+class CommandChannelCooldownDao(val driverManager: DriverManager) : Dao(driverManager) {
+    override val table: String = "commandChannelCooldowns"
+    override val tableStructure: String = "guildId bigint, channelId bigint, commandId int, cooldownMillis long"
+    override val keys: String = "UNIQUE KEY (channelId, commandId)"
+
+    init {
+        driverManager.registerTable(table, tableStructure, keys)
+    }
+
+    fun getCooldownMapForChannel(channelId: Long, cooldownMap: Consumer<Map<Int, Long>>) {
+        driverManager.executeQuery("SELECT * FROM $table WHERE channelId = ?", Consumer {
+            val map = HashMap<Int, Long>()
+            while (it.next()) {
+                map[it.getInt("commandId")] = it.getLong("cooldownMillis")
+            }
+            cooldownMap.accept(map)
+        }, channelId)
+    }
 }
--- src/main/java/me/melijn/melijnbot/database/cooldown/CommandChannelCooldownWrapper.kt
+++ src/main/java/me/melijn/melijnbot/database/cooldown/CommandChannelCooldownWrapper.kt
@@ -1,4 +1,28 @@
 package me.melijn.melijnbot.database.cooldown
 
-class CommandChannelCooldownWrapper {
+import com.github.benmanes.caffeine.cache.Caffeine
+import me.melijn.melijnbot.database.IMPORTANT_CACHE
+import me.melijn.melijnbot.objects.threading.TaskManager
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
+import java.util.function.Consumer
+
+class CommandChannelCooldownWrapper(val taskManager: TaskManager, val commandChannelCooldownDao: CommandChannelCooldownDao) {
+
+    val commandChannelCooldownCache = Caffeine.newBuilder()
+            .expireAfterAccess(IMPORTANT_CACHE, TimeUnit.MINUTES)
+            .executor(taskManager.getExecutorService())
+            .buildAsync<Long, Map<Int, Long>>() { key, executor -> }
+
+    fun getCommancChannelCooldowns(channelId: Long, executor: Executor = taskManager.getExecutorService()): CompletableFuture<Map<Int, Long>> {
+        val map = CompletableFuture<Map<Int, Long>>()
+        executor.execute {
+            commandChannelCooldownDao.getCooldownMapForChannel(channelId, Consumer {
+                map.complete(it)
+            })
+        }
+        return map
+    }
+
 }
