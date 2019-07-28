efcd19052b3ccd9b4e3ef9d94c8075bb4b9a354a
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-07-28 17:39:46 +0200

    Added JDAUtils, utils to retrieve users ect from arguments

---
+++ src/main/java/me/melijn/melijnbot/objects/utils/JDAUtils.kt
@@ -0,0 +1,29 @@
+package me.melijn.melijnbot.objects.utils
+
+import me.melijn.melijnbot.objects.command.CommandContext
+import net.dv8tion.jda.api.entities.User
+
+fun getUserByArgs(context: CommandContext, index: Int): User {
+    var user = getUserByArgsN(context, index)
+    if (user == null) user = context.getAuthor()
+    return user
+}
+
+
+fun getUserByArgsN(context: CommandContext, index: Int): User? {//With null
+    var user: User? = null
+    if (context.args.size > index) {
+        val arg = context.args[index]
+
+        user = if (context.getMessage().mentionedUsers.size > context.offset)
+            context.getMessage().mentionedUsers[context.offset]
+        else if (arg.matches(Regex("\\d+")) && context.jda.shardManager?.getUserById(arg) != null)
+            context.jda.shardManager?.getUserById(arg)
+        else if (context.isFromGuild && context.getGuild().getMembersByName(arg, true).size > 0)
+            context.getGuild().getMembersByName(arg, true)[0].user
+        else if (context.isFromGuild && context.getGuild().getMembersByNickname(arg, true).size > 0)
+            context.getGuild().getMembersByNickname(arg, true)[0].user
+        else user
+    }
+    return user
+}
