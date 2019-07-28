89c634430d1b373354ffd5cb95933655fed0da65
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-07-28 22:14:07 +0200

    Added getRoleByArgsN method

--- src/main/java/me/melijn/melijnbot/objects/utils/JDAUtils.kt
+++ src/main/java/me/melijn/melijnbot/objects/utils/JDAUtils.kt
@@ -1,7 +1,9 @@
 package me.melijn.melijnbot.objects.utils
 
 import me.melijn.melijnbot.objects.command.CommandContext
+import net.dv8tion.jda.api.entities.Role
 import net.dv8tion.jda.api.entities.User
+import java.util.regex.Pattern
 
 fun getUserByArgs(context: CommandContext, index: Int): User {
     var user = getUserByArgsN(context, index)
@@ -26,4 +28,29 @@ fun getUserByArgsN(context: CommandContext, index: Int): User? {//With null
         else user
     }
     return user
+}
+
+fun getRoleByArgsN(context: CommandContext, index: Int): Role? {
+    var role: Role? = null
+    if (context.args.size > index) {
+        val arg = context.args[index]
+
+        role = if (arg.matches(Regex("\\d+")) && context.jda.shardManager?.getRoleById(arg) != null)
+            context.jda.shardManager?.getRoleById(arg)
+        else if (context.isFromGuild && context.getGuild().getRolesByName(arg, true).size > 0)
+            context.getGuild().getRolesByName(arg, true)[0]
+        else if (arg.matches(Regex("<@&\\d+>"))) {
+            var role2: Role? = null
+            val pattern = Pattern.compile("<@&(\\d+)>")
+            val matcher = pattern.matcher(arg)
+            while (matcher.find()) {
+                val id = matcher.group(1)
+                val role3 = context.jda.shardManager?.getRoleById(id)
+                if (role2 != null && role3 == null) continue
+                role2 = role3
+            }
+            role2
+        } else role
+    }
+    return role
 }
