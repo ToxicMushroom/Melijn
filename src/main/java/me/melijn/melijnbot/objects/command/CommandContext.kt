f0046fc9cd8742b652c8191ae73b5d10f3785539
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-07-28 17:40:22 +0200

    Added offset

--- src/main/java/me/melijn/melijnbot/objects/command/CommandContext.kt
+++ src/main/java/me/melijn/melijnbot/objects/command/CommandContext.kt
@@ -3,6 +3,7 @@ package me.melijn.melijnbot.objects.command
 import me.melijn.melijnbot.Container
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import java.util.regex.Pattern
 
 class CommandContext(
         private val messageReceivedEvent: MessageReceivedEvent,
@@ -20,6 +21,7 @@ class CommandContext(
     }
 
     val usedPrefix: String = commandParts[0]
+    val offset: Int = retrieveOffset()
     val args: List<String> = commandParts.drop(2)
     val embedColor: Int = container.settings.embedColor
     val prefix: String = container.settings.prefix
@@ -32,4 +34,15 @@ class CommandContext(
     val authorId = getAuthor().idLong
 
     fun getCommands() = commandList
+    private fun retrieveOffset(): Int {
+        var count = 0
+        val pattern = Pattern.compile("<@!?(\\d+)>")
+        val matcher = pattern.matcher(commandParts[0])
+
+        while (matcher.find()) {
+            if (jda.getUserById(matcher.group(1)) != null) count++
+        }
+
+        return count
+    }
 }
