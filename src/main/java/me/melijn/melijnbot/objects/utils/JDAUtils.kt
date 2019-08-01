8a38732d6750f860401078c50257513bbc6e5a61
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-08-01 21:27:52 +0200

    Added getTextChannelByArgsN and improved getRoleByArgsN to support sameGuildAsContext as an option

--- src/main/java/me/melijn/melijnbot/objects/utils/JDAUtils.kt
+++ src/main/java/me/melijn/melijnbot/objects/utils/JDAUtils.kt
@@ -2,6 +2,7 @@ package me.melijn.melijnbot.objects.utils
 
 import me.melijn.melijnbot.objects.command.CommandContext
 import net.dv8tion.jda.api.entities.Role
+import net.dv8tion.jda.api.entities.TextChannel
 import net.dv8tion.jda.api.entities.User
 import java.util.regex.Pattern
 
@@ -30,8 +31,9 @@ fun getUserByArgsN(context: CommandContext, index: Int): User? {//With null
     return user
 }
 
-fun getRoleByArgsN(context: CommandContext, index: Int): Role? {
+fun getRoleByArgsN(context: CommandContext, index: Int, sameGuildAsContext: Boolean = true): Role? {
     var role: Role? = null
+    if (!context.isFromGuild && sameGuildAsContext) return role
     if (context.args.size > index) {
         val arg = context.args[index]
 
@@ -52,5 +54,33 @@ fun getRoleByArgsN(context: CommandContext, index: Int): Role? {
             role2
         } else role
     }
+    if (sameGuildAsContext && !context.getGuild().roles.contains(role)) return null
     return role
+}
+
+fun getTextChannelByArgsN(context: CommandContext, index: Int, sameGuildAsContext: Boolean = true): TextChannel? {
+    var channel: TextChannel? = null
+    if (!context.isFromGuild && sameGuildAsContext) return channel
+    if (context.args.size > index && context.isFromGuild) {
+        val arg = context.args[index]
+
+        channel = if (arg.matches(Regex("\\d+"))) {
+            context.jda.shardManager?.getTextChannelById(arg)
+        } else if (context.isFromGuild && context.getGuild().getTextChannelsByName(arg, true).size > 0) {
+            context.getGuild().getTextChannelsByName(arg, true)[0]
+        } else if (arg.matches(Regex("<#\\d+>"))) {
+            var textChannel1: TextChannel? = null
+            val pattern = Pattern.compile("<#(\\d+)>")
+            val matcher = pattern.matcher(arg)
+            while (matcher.find()) {
+                val id = matcher.group(1)
+                val textChannel2 = context.jda.shardManager?.getTextChannelById(id)
+                if (textChannel1 != null && textChannel2 == null) continue
+                textChannel1 = textChannel2
+            }
+            textChannel1
+        } else channel
+    }
+    if (sameGuildAsContext && !context.getGuild().textChannels.contains(channel)) return null
+    return channel
 }
