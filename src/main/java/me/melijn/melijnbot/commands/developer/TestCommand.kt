9f5268879a6f185506f62b0087c0dff5bfbb0681
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-08-10 21:52:03 +0200

    Added testcommand

---
+++ src/main/java/me/melijn/melijnbot/commands/developer/TestCommand.kt
@@ -0,0 +1,25 @@
+package me.melijn.melijnbot.commands.developer
+
+import me.melijn.melijnbot.enums.LogChannelType
+import me.melijn.melijnbot.objects.command.AbstractCommand
+import me.melijn.melijnbot.objects.command.CommandCategory
+import me.melijn.melijnbot.objects.command.CommandContext
+import me.melijn.melijnbot.objects.utils.sendMsg
+
+class TestCommand : AbstractCommand("command.test") {
+
+    init {
+        id = 20
+        name = "test"
+        commandCategory = CommandCategory.DEVELOPER
+    }
+
+    override fun execute(context: CommandContext) {
+        val timeStamp1 = System.currentTimeMillis()
+        val future = context.daoManager.logChannelWrapper.logChannelCache.get(Pair(context.guildId, LogChannelType.PERMANENT_BAN))
+        val timeStamp2 = System.currentTimeMillis()
+        val channel = future.get()
+        val timeStamp3 = System.currentTimeMillis()
+        sendMsg(context, "${timeStamp2 - timeStamp1} , ${timeStamp3 - timeStamp2} , $channel")
+    }
+}
