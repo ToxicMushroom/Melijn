5bd95bb5ab7bdb4c5f37d852a3512bebb119dfb5
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-08-14 16:43:45 +0200

    setLogChannelCommand is ready for testing

--- src/main/java/me/melijn/melijnbot/commands/administration/SetLogChannelCommand.kt
+++ src/main/java/me/melijn/melijnbot/commands/administration/SetLogChannelCommand.kt
@@ -4,10 +4,11 @@ import me.melijn.melijnbot.enums.LogChannelType
 import me.melijn.melijnbot.objects.command.AbstractCommand
 import me.melijn.melijnbot.objects.command.CommandCategory
 import me.melijn.melijnbot.objects.command.CommandContext
-import me.melijn.melijnbot.objects.embed.Embedder
 import me.melijn.melijnbot.objects.translation.Translateable
-import me.melijn.melijnbot.objects.utils.*
-import net.dv8tion.jda.api.utils.MarkdownUtil
+import me.melijn.melijnbot.objects.utils.asTag
+import me.melijn.melijnbot.objects.utils.getTextChannelByArgsNMessage
+import me.melijn.melijnbot.objects.utils.sendMsg
+import me.melijn.melijnbot.objects.utils.sendSyntax
 
 class SetLogChannelCommand : AbstractCommand("command.setlogchannel") {
 
@@ -29,8 +30,66 @@ class SetLogChannelCommand : AbstractCommand("command.setlogchannel") {
             sendSyntax(context, syntax)
             return
         }
+        if (matchingEnums.size > 1)
+            handleEnums(context, matchingEnums)
+        else handleEnum(context, matchingEnums[0])
 
-        handleEnums(context, matchingEnums)
+    }
+
+    private fun handleEnum(context: CommandContext, logChannelType: LogChannelType) {
+        if (context.args.size > 1) {
+            setChannel(context, logChannelType)
+        } else {
+            displayChannel(context, logChannelType)
+        }
+    }
+
+    private fun displayChannel(context: CommandContext, logChannelType: LogChannelType) {
+        val daoWrapper = context.daoManager.logChannelWrapper
+        val pair = Pair(context.guildId, logChannelType)
+        val channelId = daoWrapper.logChannelCache.get(pair).get()
+        val channel = context.getGuild().getTextChannelById(channelId)
+
+        if (channelId != -1L && channel == null) {
+            daoWrapper.removeChannel(pair.first, pair.second)
+            return
+        }
+
+        val msg = (if (channel != null) {
+            Translateable("$root.show.set.single").string(context)
+                    .replace("%channel%", channel.asTag)
+        } else {
+            Translateable("$root.show.unset.single").string(context)
+        }).replace("%logChannelType%", logChannelType.text)
+
+        sendMsg(context, msg)
+    }
+
+
+
+    private fun setChannel(context: CommandContext, logChannelType: LogChannelType) {
+        if (context.args.size < 2) {
+            sendSyntax(context, syntax)
+            return
+        }
+
+        val daoWrapper = context.daoManager.logChannelWrapper
+        val msg = if (context.args[1].equals("null", true)) {
+
+            daoWrapper.removeChannel(context.guildId, logChannelType)
+
+            Translateable("$root.unset.single").string(context)
+                    .replace("%logChannelType%", logChannelType.text)
+        } else {
+            val channel = getTextChannelByArgsNMessage(context, 1) ?: return
+            daoWrapper.setChannel(context.guildId, logChannelType, channel.idLong)
+
+            Translateable("$root.set.single").string(context)
+                    .replace("%logChannelType%", logChannelType.text)
+                    .replace("%channel%", channel.asTag)
+
+        }
+        sendMsg(context, msg)
     }
 
     private fun handleEnums(context: CommandContext, logChannelTypes: List<LogChannelType>) {
@@ -43,21 +102,24 @@ class SetLogChannelCommand : AbstractCommand("command.setlogchannel") {
 
     private fun displayChannels(context: CommandContext, logChannelTypes: List<LogChannelType>) {
         val daoWrapper = context.daoManager.logChannelWrapper
-        val title = Translateable("$root.settings").string(context)
+        val title = Translateable("$root.show.multple").string(context)
+                .replace("%channelCount%", logChannelTypes.size.toString())
                 .replace("%logChannelTypeNode%", context.args[0])
+
         val lines = emptyList<String>().toMutableList()
-        val eb = Embedder(context)
+
         for (type in logChannelTypes) {
             val pair = Pair(context.guildId, type)
             val channelId = daoWrapper.logChannelCache.get(pair).get()
             val channel = context.getGuild().getTextChannelById(channelId)
 
             if (channelId != -1L && channel == null) daoWrapper.removeChannel(pair.first, pair.second)
-            lines += MarkdownUtil.bold(type.text) + " " + (channel?.asMention ?: "/")
+            lines += "${type.text}: " + (channel?.asMention ?: "/")
         }
-        val content = lines.joinToString("\n")
-        eb.addField(title, content, false)
-        sendEmbed(context, eb.build())
+
+        val content = lines.joinToString(separator = "\n", prefix = "\n")
+        val msg = title + content
+        sendMsg(context, msg)
     }
 
     private fun setChannels(context: CommandContext, logChannelTypes: List<LogChannelType>) {
@@ -67,18 +129,18 @@ class SetLogChannelCommand : AbstractCommand("command.setlogchannel") {
         }
 
         val daoWrapper = context.daoManager.logChannelWrapper
-        val msg =if (context.args[1].equals("null", true)) {
+        val msg = if (context.args[1].equals("null", true)) {
 
             daoWrapper.removeChannels(context.guildId, logChannelTypes)
 
-            Translateable("$root.unset").string(context)
+            Translateable("$root.unset.multiple").string(context)
                     .replace("%channelCount%", logChannelTypes.size.toString())
                     .replace("%logChannelNode%", context.args[0])
         } else {
             val channel = getTextChannelByArgsNMessage(context, 1) ?: return
             daoWrapper.setChannels(context.guildId, logChannelTypes, channel.idLong)
 
-            Translateable("$root.set").string(context)
+            Translateable("$root.set.multiple").string(context)
                     .replace("%channelCount%", logChannelTypes.size.toString())
                     .replace("%logChannelNode%", context.args[0])
                     .replace("%channel%", channel.asTag)
