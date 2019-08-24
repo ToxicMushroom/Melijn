244419b846717861c1ab36b48ba34f7bdeefa148
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-08-24 23:57:13 +0200

    Added String Constants for reused strings

---
+++ src/main/java/me/melijn/melijnbot/objects/translation/StringConstants.kt
@@ -0,0 +1,6 @@
+package me.melijn.melijnbot.objects.translation
+
+//Introduction to constants
+const val MESSAGE_UNKNOWN_USER = "message.unknown.user"
+const val MESSAGE_UNKNOWN_PERMISSIONNODE = "message.unknown.permissionnode"
+const val MESSAGE_UNKNOWN_PERMSTATE = "message.unknown.permstate"
