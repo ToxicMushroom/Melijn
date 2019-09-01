003dc98920a158612198512b523cfe0938a33f22
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-09-01 20:46:03 +0200

    use coroutines as test

--- src/main/java/me/melijn/melijnbot/objects/threading/TaskManager.kt
+++ src/main/java/me/melijn/melijnbot/objects/threading/TaskManager.kt
@@ -1,17 +1,22 @@
 package me.melijn.melijnbot.objects.threading
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.launch
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit
 
 
-class TaskManager {
 
-    private val threadFactory = { name: String -> ThreadFactoryBuilder().setNameFormat("[$name-Pool-%d] ").build() }
-    private val executorService = Executors.newCachedThreadPool(threadFactory.invoke("Task"))
-    private val scheduledExecutorService = Executors.newScheduledThreadPool(15, threadFactory.invoke("Repeater"))
+
+class TaskManager {
+    val threadFactory = { name: String -> ThreadFactoryBuilder().setNameFormat("[$name-Pool-%d] ").build() }
+    val executorService: ExecutorService = Executors.newCachedThreadPool(threadFactory.invoke("Task"))
+    val dispatcher = executorService.asCoroutineDispatcher()
+    val scheduledExecutorService: ScheduledExecutorService = Executors.newScheduledThreadPool(15, threadFactory.invoke("Repeater"))
 
     fun scheduleRepeating(runnable: Runnable, periodMillis: Long) {
         scheduledExecutorService.scheduleAtFixedRate(Task(runnable), 0, periodMillis, TimeUnit.MILLISECONDS)
@@ -22,18 +27,14 @@ class TaskManager {
     }
 
     fun async(func: () -> Unit) {
-        executorService.submit(Task(Runnable(func)))
+        CoroutineScope(dispatcher).launch {
+            val task  = Task(Runnable(func))
+            task.run()
+        }
     }
+
 
     fun asyncAfter(afterMillis: Long, func: () -> Unit) {
         scheduledExecutorService.schedule(Task(Runnable(func)), afterMillis, TimeUnit.MILLISECONDS)
-    }
-
-    fun getExecutorService(): ExecutorService {
-        return executorService
-    }
-
-    fun getScheduledExecutorService(): ScheduledExecutorService {
-        return scheduledExecutorService
     }
 }
