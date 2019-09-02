ffc4d0d4a8a9fe34a8cc40f2055460d009010a1b
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-09-02 08:00:41 +0200

    new location for putting duplicated code so it isn't all over the place in random functions

---
+++ src/main/java/me/melijn/melijnbot/commandutil/administration/MessageCommandUtil.kt
@@ -0,0 +1,44 @@
+package me.melijn.melijnbot.commandutil.administration
+
+import kotlinx.coroutines.future.await
+import me.melijn.melijnbot.enums.MessageType
+import me.melijn.melijnbot.objects.command.AbstractCommand
+import me.melijn.melijnbot.objects.command.CommandContext
+import me.melijn.melijnbot.objects.translation.Translateable
+import me.melijn.melijnbot.objects.utils.sendMsg
+
+class MessageCommandUtil{
+    companion object {
+        suspend fun setMessage(cmd: AbstractCommand, context: CommandContext, type: MessageType) {
+            val messageWrapper = context.daoManager.messageWrapper
+            val oldContent = messageWrapper.messageCache.get(Pair(context.getGuildId(), type)).await()
+            val newContent = context.rawArg
+
+            val msg = if (newContent.equals("null", true)) {
+                messageWrapper.removeMessage(context.getGuildId(), type)
+                Translateable("${cmd.root}.set.unset").string(context)
+                        .replace("%oldContent%", oldContent)
+            } else {
+                messageWrapper.setMessage(context.getGuildId(), type, newContent)
+                Translateable("${cmd.root}.set").string(context)
+                        .replace("%oldContent%", oldContent)
+                        .replace("%newContent%", newContent)
+            }
+
+            sendMsg(context, msg)
+        }
+
+        suspend fun showMessage(cmd: AbstractCommand, context: CommandContext, type: MessageType) {
+            val messageWrapper = context.daoManager.messageWrapper
+            val content = messageWrapper.messageCache.get(Pair(context.getGuildId(), type)).await()
+            val msg = if (content.isBlank()) {
+                Translateable("${cmd.root}.show.unset").string(context)
+            } else {
+                Translateable("${cmd.root}.show.set").string(context)
+                        .replace("%content%", content)
+            }
+
+            sendMsg(context, msg)
+        }
+    }
+}
