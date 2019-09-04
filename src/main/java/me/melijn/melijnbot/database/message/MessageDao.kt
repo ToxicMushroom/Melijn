3aed8a89bb09e66b1309d54911de90b4bb35cc9c
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-09-04 22:06:48 +0200

    sich

--- src/main/java/me/melijn/melijnbot/database/message/MessageDao.kt
+++ src/main/java/me/melijn/melijnbot/database/message/MessageDao.kt
@@ -3,7 +3,10 @@ package me.melijn.melijnbot.database.message
 import me.melijn.melijnbot.database.Dao
 import me.melijn.melijnbot.database.DriverManager
 import me.melijn.melijnbot.enums.MessageType
+import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.entities.MessageEmbed
+import net.dv8tion.jda.api.utils.data.DataObject
+import net.dv8tion.jda.internal.JDAImpl
 import org.json.JSONObject
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
@@ -11,7 +14,7 @@ import kotlin.coroutines.suspendCoroutine
 class MessageDao(driverManager: DriverManager) : Dao(driverManager) {
 
     override val table: String = "messages"
-    override val tableStructure: String = "guildId bigint, type varchar(32), message varchar(2048)"
+    override val tableStructure: String = "guildId bigint, type varchar(32), message varchar(4096)"
     override val keys: String = "UNIQUE KEY(guildId, type)"
 
     init {
@@ -23,11 +26,11 @@ class MessageDao(driverManager: DriverManager) : Dao(driverManager) {
                 guildId, type.toString(), message, message)
     }
 
-    suspend fun get(guildId: Long, type: MessageType): String = suspendCoroutine {
+    suspend fun get(guildId: Long, type: MessageType): String? = suspendCoroutine {
         driverManager.executeQuery("SELECT * FROM $table WHERE guildId = ? AND type = ?", { rs ->
             if (rs.next()) {
-                it.resume(rs.getString("content"))
-            } else it.resume("")
+                it.resume(rs.getString("message"))
+            } else it.resume(null)
         }, guildId, type.toString())
     }
 
@@ -37,21 +40,45 @@ class MessageDao(driverManager: DriverManager) : Dao(driverManager) {
     }
 }
 
-data class ModularMessage(val messageContent: String? = null,
-                          val embed: MessageEmbed? = null,
-                          val attachments: List<String> = emptyList()) {
+data class ModularMessage(var messageContent: String? = null,
+                          var embed: MessageEmbed? = null,
+                          var attachments: List<String> = emptyList()) {
     fun toJSON(): String {
         val json = JSONObject()
         messageContent?.let { json.put("content", it) }
-        embed?.let {
-            val embedJson = JSONObject()
-            embedJson.put("title", )
+        embed?.let { membed ->
+            json.put("embed", membed.toData().toString())
+        }
 
-            json.put("embed", embedJson)
-        }
-        attachments.let {
-            json.put("attachments", it.joinToString("%SPLIT%"))
-        }
+        json.put("attachments", attachments.joinToString("%SPLIT%"))
         return json.toString(4)
+    }
+
+    companion object {
+        fun fromJSON(jda: JDA, json: String): ModularMessage {
+            try {
+                val jsonObj = JSONObject(json)
+                var content: String? = null
+                if (jsonObj.has("content")) {
+                    content = jsonObj.getString("content")
+                }
+                var embed: MessageEmbed? = null
+                if (jsonObj.has("embed")) {
+                    val jdaImpl = (jda as JDAImpl)
+                    val embedString = jsonObj.getString("embed")
+                    val dataObject = DataObject.fromJson(embedString)
+                    embed = jdaImpl.entityBuilder.createMessageEmbed(dataObject)
+                }
+                val attachments = mutableListOf<String>()
+                if (jsonObj.has("attachments")) {
+                    val attachmentsString = jsonObj.getString("attachments")
+                    val attachmentStrings = attachmentsString.split("%SPLIT%")
+                    attachments.addAll(attachmentStrings)
+                }
+                return ModularMessage(content, embed, attachments)
+            } catch (e: Exception) {
+                throw IllegalArgumentException("Invalid JSON structure")
+            }
+        }
     }
 }
--- src/main/java/me/melijn/melijnbot/database/message/MessageWrapper.kt
+++ src/main/java/me/melijn/melijnbot/database/message/MessageWrapper.kt
@@ -2,37 +2,52 @@ package me.melijn.melijnbot.database.message
 
 import com.github.benmanes.caffeine.cache.Caffeine
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.future.await
 import kotlinx.coroutines.launch
 import me.melijn.melijnbot.database.IMPORTANT_CACHE
 import me.melijn.melijnbot.enums.MessageType
 import me.melijn.melijnbot.objects.threading.TaskManager
+import net.dv8tion.jda.api.JDA
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.TimeUnit
 
-class MessageWrapper (val taskManager: TaskManager, private val messageDao: MessageDao) {
+class MessageWrapper (val taskManager: TaskManager, private val jda: JDA, private val messageDao: MessageDao) {
 
     val messageCache = Caffeine.newBuilder()
             .executor(taskManager.executorService)
             .expireAfterAccess(IMPORTANT_CACHE, TimeUnit.MINUTES)
-            .buildAsync<Pair<Long, MessageType>, String> { key, _ ->
+            .buildAsync<Pair<Long, MessageType>, ModularMessage?> { key, _ ->
                 getMessage(key)
             }
 
-    private fun getMessage(pair: Pair<Long, MessageType>): CompletableFuture<String> {
-        val future = CompletableFuture<String>()
+    private fun getMessage(pair: Pair<Long, MessageType>): CompletableFuture<ModularMessage?> {
+        val future = CompletableFuture<ModularMessage?>()
         CoroutineScope(taskManager.dispatcher).launch {
-            future.complete(messageDao.get(pair.first, pair.second))
+            val json = messageDao.get(pair.first, pair.second)
+            if (json == null) {
+                future.complete(null)
+                return@launch
+            }
+            try {
+                future.complete(ModularMessage.fromJSON(jda, json))
+            } catch (e: Exception) {
+                e.printStackTrace()
+                future.complete(null)
+            }
+
         }
         return future
     }
 
-    fun setMessage(guildId: Long, messageType: MessageType, content: String) {
-        messageDao.set(guildId, messageType, content)
-        messageCache.put(Pair(guildId, messageType), CompletableFuture.completedFuture(content))
+    suspend fun setMessageContent(guildId: Long, messageType: MessageType, content: String?) {
+        val message = messageCache.get(Pair(guildId, messageType)).await() ?: ModularMessage()
+        message.messageContent = content
+        messageDao.set(guildId, messageType, message.toJSON())
+        messageCache.put(Pair(guildId, messageType), CompletableFuture.completedFuture(message))
     }
 
     fun removeMessage(guildId: Long, type: MessageType) {
         messageDao.remove(guildId, type)
-        messageCache.put(Pair(guildId, type), CompletableFuture.completedFuture(""))
+        messageCache.put(Pair(guildId, type), CompletableFuture.completedFuture(null))
     }
 }
