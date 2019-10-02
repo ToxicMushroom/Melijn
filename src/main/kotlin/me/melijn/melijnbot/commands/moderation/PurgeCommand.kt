b27483d328e2df13bcea276bf58680b9e41a99e3
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-10-02 14:19:11 +0200

    progress

--- src/main/kotlin/me/melijn/melijnbot/commands/moderation/PurgeCommand.kt
+++ src/main/kotlin/me/melijn/melijnbot/commands/moderation/PurgeCommand.kt
@@ -3,8 +3,12 @@ package me.melijn.melijnbot.commands.moderation
 import me.melijn.melijnbot.objects.command.AbstractCommand
 import me.melijn.melijnbot.objects.command.CommandCategory
 import me.melijn.melijnbot.objects.command.CommandContext
+import me.melijn.melijnbot.objects.translation.i18n
+import me.melijn.melijnbot.objects.utils.await
 import me.melijn.melijnbot.objects.utils.getIntegerFromArgNMessage
+import me.melijn.melijnbot.objects.utils.sendMsg
 import me.melijn.melijnbot.objects.utils.sendSyntax
+import java.util.concurrent.TimeUnit
 
 class PurgeCommand : AbstractCommand("command.purge") {
 
@@ -21,6 +25,15 @@ class PurgeCommand : AbstractCommand("command.purge") {
             return
         }
 
-        val amount = getIntegerFromArgNMessage(context, 0, 1, 1000)
+        val amount = getIntegerFromArgNMessage(context, 0, 1, 1000) ?: return
+        val language = context.getLanguage()
+        val messages = context.getTextChannel().history.retrievePast(amount).await()
+
+        context.getTextChannel().purgeMessages(messages)
+        val msg = i18n.getTranslation(language, "$root.success")
+            .replace("%amount%", amount.toString())
+
+
+        sendMsg(context, msg)[0].delete().queueAfter(5, TimeUnit.SECONDS)
     }
 }
