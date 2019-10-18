009bd93b4842255d01f32fb4e60f681a905b2576
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-10-18 07:19:05 +0200

    Fixed indentation and added 2 extra variables

--- src/main/kotlin/me/melijn/melijnbot/objects/jagtag/DiscordMethods.kt
+++ src/main/kotlin/me/melijn/melijnbot/objects/jagtag/DiscordMethods.kt
@@ -11,62 +11,69 @@ import net.dv8tion.jda.api.entities.User
 
 object DiscordMethods {
     fun getMethods(): List<Method> = listOf(
-            Method("user", { env ->
-                val user: User = env.getReifiedX("user")
-                user.asTag
-            }),
+        Method("user", { env ->
+            val user: User = env.getReifiedX("user")
+            user.asTag
+        }),
 
 
+        //{isUser:userId|userTag}
+        Method("isUser", { "true" }, { env: Environment, input: Array<String> ->
+            val guild: Guild = env.getReifiedX("guild")
+            val arg = input[0]
 
-            //{isUser:userId|userTag}
-            Method("isUser", { "true" }, { env: Environment, input: Array<String> ->
-                val guild: Guild = env.getReifiedX("guild")
-                val arg = input[0]
+            (retrieveUserByArgsN(guild, arg) != null).toString()
+        }),
 
-                (retrieveUserByArgsN(guild, arg) != null).toString()
-            }),
-
-            //{isMember:userId|userTag}
-            Method("isMember", { "true" }, { env: Environment, input: Array<String> ->
-                val guild: Guild = env.getReifiedX("guild")
-                val arg = input[0]
-                (getMemberByArgsN(guild, arg) != null).toString()
-            }),
-            Method("userId", { env ->
-                val user: User = env.getReifiedX("user")
-                user.id
-            }),
-            Method("username", { env ->
-                val user: User = env.getReifiedX("user")
-                user.name
-            }),
-            Method("nickname", { env ->
-                val member: Member = env.getReifiedX("member")
-                member.nickname ?: throw ParseException("no nickname")
-            }),
-            Method("hasNickname", { env ->
-                val member: Member = env.getReifiedX("member")
-                (member.nickname != null).toString()
-            }),
-            Method("effectiveName", { env ->
-                val member: Member = env.getReifiedX("member")
-                member.effectiveName
-            }),
-            Method("effectiveAvatarUrl", { env ->
-                val user: User = env.getReifiedX("user")
-                user.effectiveAvatarUrl
-            }),
-            Method("discrim", { env ->
-                val user: User = env.getReifiedX("user")
-                user.discriminator
-            }),
-            Method("guildname", { env ->
-                val guild: Guild = env.getReifiedX("guild")
-                guild.name
-            }),
-            Method("membercount", { env ->
-                val guild: Guild = env.getReifiedX("guild")
-                guild.memberCache.size().toString()
-            })
+        //{isMember:userId|userTag}
+        Method("isMember", { "true" }, { env: Environment, input: Array<String> ->
+            val guild: Guild = env.getReifiedX("guild")
+            val arg = input[0]
+            (getMemberByArgsN(guild, arg) != null).toString()
+        }),
+        Method("userId", { env ->
+            val user: User = env.getReifiedX("user")
+            user.id
+        }),
+        Method("username", { env ->
+            val user: User = env.getReifiedX("user")
+            user.name
+        }),
+        Method("voiceChannelId", { env ->
+            val member: Member = env.getReifiedX("member")
+            member.voiceState?.channel?.id ?: "null"
+        }),
+        Method("nickname", { env ->
+            val member: Member = env.getReifiedX("member")
+            member.nickname ?: throw ParseException("no nickname")
+        }),
+        Method("hasNickname", { env ->
+            val member: Member = env.getReifiedX("member")
+            (member.nickname != null).toString()
+        }),
+        Method("effectiveName", { env ->
+            val member: Member = env.getReifiedX("member")
+            member.effectiveName
+        }),
+        Method("effectiveAvatarUrl", { env ->
+            val user: User = env.getReifiedX("user")
+            user.effectiveAvatarUrl
+        }),
+        Method("discrim", { env ->
+            val user: User = env.getReifiedX("user")
+            user.discriminator
+        }),
+        Method("guildName", { env ->
+            val guild: Guild = env.getReifiedX("guild")
+            guild.name
+        }),
+        Method("guildId", { env ->
+            val guild: Guild = env.getReifiedX("guild")
+            guild.id
+        }),
+        Method("membercount", { env ->
+            val guild: Guild = env.getReifiedX("guild")
+            guild.memberCache.size().toString()
+        })
     )
 }
