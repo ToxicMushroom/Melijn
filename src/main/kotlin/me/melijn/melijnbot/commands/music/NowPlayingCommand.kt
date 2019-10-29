58745954fe0832e49b9c982df1a8edc9fb6db936
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-10-29 17:06:04 +0100

    Added NowPlayingCommand.kt, VolumeCommand.kt

---
+++ src/main/kotlin/me/melijn/melijnbot/commands/music/NowPlayingCommand.kt
@@ -0,0 +1,55 @@
+package me.melijn.melijnbot.commands.music
+
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack
+import me.melijn.melijnbot.objects.command.AbstractCommand
+import me.melijn.melijnbot.objects.command.CommandCategory
+import me.melijn.melijnbot.objects.command.CommandContext
+import me.melijn.melijnbot.objects.command.RunCondition
+import me.melijn.melijnbot.objects.embed.Embedder
+import me.melijn.melijnbot.objects.translation.i18n
+import me.melijn.melijnbot.objects.utils.getDurationString
+import me.melijn.melijnbot.objects.utils.sendEmbed
+
+class NowPlayingCommand : AbstractCommand("command.nowplaying") {
+
+    init {
+        id = 88
+        name = "nowPlaying"
+        aliases = arrayOf("np", "playing", "currentTrack", "currentSong")
+        runConditions = arrayOf(RunCondition.PLAYING_TRACK_NOT_NULL)
+        commandCategory = CommandCategory.MUSIC
+    }
+
+    override suspend fun execute(context: CommandContext) {
+        val trackManager = context.getGuildMusicPlayer().guildTrackManager
+        val playingTrack = trackManager.iPlayer.playingTrack
+        val status = i18n.getTranslation(context, if (trackManager.iPlayer.isPaused) "paused" else "playing")
+        val title = i18n.getTranslation(context, "$root.title")
+            .replace("%status%", status)
+
+        val description = i18n.getTranslation(context, "$root.show.description")
+            .replace("%title%", playingTrack.info.title)
+            .replace("%url%", playingTrack.info.uri)
+        val progressField = i18n.getTranslation(context, "$root.progress")
+        val thumbnail = "https://img.youtube.com/vi/${playingTrack.identifier}/hqdefault.jpg"
+
+        val eb = Embedder(context)
+        eb.setTitle(title)
+        eb.setThumbnail(thumbnail)
+        eb.setDescription(description)
+        eb.addField(progressField, getProgressBar(trackManager.iPlayer.playingTrack, trackManager.iPlayer.trackPosition), false)
+        sendEmbed(context, eb.build())
+    }
+
+    private fun getProgressBar(playingTrack: AudioTrack, playerPosition: Long): String {
+        if (playingTrack.info.isStream) {
+            return "**" + getDurationString(playerPosition) + " | \uD83D\uDD34 Live**"
+        }
+        val percent = (playerPosition.toDouble() / playingTrack.duration.toDouble() * 18.0).toInt()
+        val sb = StringBuilder("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬")
+        sb.insert(percent, "](${playingTrack.info.uri + "&t=" + (playerPosition / 1000)})<a:cool_nyan:490978764264570894>")
+        sb.append(" **").append(getDurationString(playerPosition)).append("/").append(getDurationString(playingTrack.duration)).append("**")
+        sb.insert(0, "[")
+        return sb.toString()
+    }
+}
--- src/main/kotlin/me/melijn/melijnbot/commands/music/VolumeCommand.kt
+++ src/main/kotlin/me/melijn/melijnbot/commands/music/VolumeCommand.kt
@@ -1,16 +1,36 @@
 package me.melijn.melijnbot.commands.music
 
 import me.melijn.melijnbot.objects.command.AbstractCommand
+import me.melijn.melijnbot.objects.command.CommandCategory
 import me.melijn.melijnbot.objects.command.CommandContext
+import me.melijn.melijnbot.objects.command.RunCondition
+import me.melijn.melijnbot.objects.translation.i18n
+import me.melijn.melijnbot.objects.utils.getIntegerFromArgNMessage
+import me.melijn.melijnbot.objects.utils.sendMsg
 
 class VolumeCommand : AbstractCommand("command.volume") {
 
     init {
         id = 87
         name = "volume"
+        aliases = arrayOf("vol")
+        runConditions = arrayOf(RunCondition.VC_BOT_ALONE_OR_USER_DJ, RunCondition.PLAYING_TRACK_NOT_NULL)
+        commandCategory = CommandCategory.MUSIC
     }
 
     override suspend fun execute(context: CommandContext) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (context.args.isEmpty()) {
+            val amount = context.getGuildMusicPlayer().guildTrackManager.iPlayer.volume
+            val msg = i18n.getTranslation(context, "$root.show")
+                .replace("%volume%", amount.toString())
+            sendMsg(context, msg)
+            return
+        }
+        val amount = getIntegerFromArgNMessage(context, 0, 0, 1000) ?: return
+        context.getGuildMusicPlayer().guildTrackManager.iPlayer.volume = amount
+
+        val msg = i18n.getTranslation(context, "$root.set")
+            .replace("%volume%", amount.toString())
+        sendMsg(context, msg)
     }
 }
