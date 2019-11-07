b38dfae4aa93e6cd87dff801394c1b79ce1711a6
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-11-07 07:44:16 +0100

    files

---
+++ src/main/kotlin/me/melijn/melijnbot/database/filter/ChannelFilterWrapper.kt
@@ -0,0 +1,4 @@
+package me.melijn.melijnbot.database.filter
+
+class ChannelFilterWrapper {
+}
--- src/main/kotlin/me/melijn/melijnbot/database/filter/FilterDao.kt
+++ src/main/kotlin/me/melijn/melijnbot/database/filter/FilterDao.kt
@@ -3,6 +3,8 @@ package me.melijn.melijnbot.database.filter
 import me.melijn.melijnbot.database.Dao
 import me.melijn.melijnbot.database.DriverManager
 import me.melijn.melijnbot.enums.FilterType
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class FilterDao(driverManager: DriverManager) : Dao(driverManager) {
 
@@ -17,5 +19,15 @@ class FilterDao(driverManager: DriverManager) : Dao(driverManager) {
     suspend fun add(guildId: Long, type: FilterType, filter: String) {
         driverManager.executeUpdate("INSERT INTO $table (guildId, type, filter) VALUES (?, ?, ?) ON CONFLICT ($primaryKey) DO NOTHING",
             guildId, type, filter)
+    }
+
+    suspend fun get(guildId: Long, type: FilterType): List<String> = suspendCoroutine{
+        driverManager.executeQuery("SELECT * FROM $table WHERE guildId = ? AND type = ?", { rs ->
+            val filters = mutableListOf<String>()
+            while (rs.next()) {
+                filters.add(rs.getString("filter"))
+            }
+            it.resume(filters)
+        }, guildId, type)
     }
 }
