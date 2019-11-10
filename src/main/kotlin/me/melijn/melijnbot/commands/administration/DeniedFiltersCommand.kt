dfc92f7d4a30cb855744aa8a8929e0eb7050d3e6
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-11-10 15:39:42 +0100

    Added DeniedFiltersCommand

---
+++ src/main/kotlin/me/melijn/melijnbot/commands/administration/DeniedFiltersCommand.kt
@@ -0,0 +1,101 @@
+package me.melijn.melijnbot.commands.administration
+
+import kotlinx.coroutines.future.await
+import me.melijn.melijnbot.enums.FilterType
+import me.melijn.melijnbot.objects.command.AbstractCommand
+import me.melijn.melijnbot.objects.command.CommandCategory
+import me.melijn.melijnbot.objects.command.CommandContext
+import me.melijn.melijnbot.objects.translation.PLACEHOLDER_CHANNEL
+import me.melijn.melijnbot.objects.translation.i18n
+import me.melijn.melijnbot.objects.utils.asTag
+import me.melijn.melijnbot.objects.utils.getTextChannelByArgsN
+import me.melijn.melijnbot.objects.utils.sendMsg
+import me.melijn.melijnbot.objects.utils.sendSyntax
+
+class DeniedFiltersCommand : AbstractCommand("command.deniedfilters") {
+
+    init {
+        id = 114
+        name = "deniedFilters"
+        children = arrayOf(AddArg(root), RemoveArg(root), ListArg(root))
+        commandCategory = CommandCategory.ADMINISTRATION
+    }
+
+    override suspend fun execute(context: CommandContext) {
+        sendSyntax(context, syntax)
+    }
+
+    class AddArg(parent: String) : AbstractCommand("$parent.add") {
+
+        init {
+            name = "add"
+            aliases = arrayOf("a")
+        }
+
+        override suspend fun execute(context: CommandContext) {
+            val channel = getTextChannelByArgsN(context, 0)
+            val denied = if (channel == null) {
+                context.rawArg
+            } else {
+                context.rawArg.replaceFirst((context.args[0] + "\\s+").toRegex(), "")
+            }
+            val wrapper = context.daoManager.filterWrapper
+            wrapper.addFilter(context.guildId, channel?.idLong, FilterType.DENIED, denied)
+
+            val part = if (channel == null) "" else ".channel"
+            val msg = i18n.getTranslation(context, "$root.success$part")
+                .replace("%filter%", denied)
+                .replace(PLACEHOLDER_CHANNEL, channel?.asTag ?: "")
+            sendMsg(context, msg)
+        }
+    }
+
+    class RemoveArg(parent: String) : AbstractCommand("$parent.remove") {
+
+        init {
+            name = "remove"
+            aliases = arrayOf("r", "delete", "d")
+        }
+
+        override suspend fun execute(context: CommandContext) {
+            val channel = getTextChannelByArgsN(context, 0)
+            val denied = if (channel == null) {
+                context.rawArg
+            } else {
+                context.rawArg.replaceFirst((context.args[0] + "\\s+").toRegex(), "")
+            }
+            val wrapper = context.daoManager.filterWrapper
+            wrapper.removeFilter(context.guildId, channel?.idLong, FilterType.DENIED, denied)
+
+            val part = if (channel == null) "" else ".channel"
+            val msg = i18n.getTranslation(context, "$root.success$part")
+                .replace("%filter%", denied)
+                .replace(PLACEHOLDER_CHANNEL, channel?.asTag ?: "")
+            sendMsg(context, msg)
+        }
+    }
+
+    class ListArg(parent: String) : AbstractCommand("$parent.list") {
+
+        init {
+            name = "list"
+            aliases = arrayOf("ls")
+        }
+
+        override suspend fun execute(context: CommandContext) {
+            val channel = getTextChannelByArgsN(context, 0)
+            val wrapper = context.daoManager.filterWrapper
+            val filters = wrapper.deniedFilterCache.get(Pair(context.guildId, channel?.idLong)).await()
+
+            val part = if (channel == null) "" else ".channel"
+            val title = i18n.getTranslation(context, "$root.success$part")
+                .replace(PLACEHOLDER_CHANNEL, channel?.asTag ?: "error")
+            var content = "```INI"
+            for ((index, filter) in filters.withIndex()) {
+                content += "\n$index - [$filter]"
+            }
+            val msg = title + content
+            sendMsg(context, msg)
+        }
+    }
+}
