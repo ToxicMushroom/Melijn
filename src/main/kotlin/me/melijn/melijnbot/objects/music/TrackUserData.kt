aa0f1f83a6bba1d8276c779229600261bd1426dc
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-11-14 22:44:34 +0100

    added currentTime

--- src/main/kotlin/me/melijn/melijnbot/objects/music/TrackUserData.kt
+++ src/main/kotlin/me/melijn/melijnbot/objects/music/TrackUserData.kt
@@ -2,7 +2,7 @@ package me.melijn.melijnbot.objects.music
 
 import net.dv8tion.jda.api.entities.User
 
-data class TrackUserData(val userId: Long, val username: String, val userDiscrim: String) {
+data class TrackUserData(val userId: Long, val username: String, val userDiscrim: String, val currentTime: Long = System.nanoTime()) {
     constructor(user: User) : this(user.idLong, user.name, user.discriminator)
 
     val userTag = "$username#$userDiscrim"
