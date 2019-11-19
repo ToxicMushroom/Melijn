f7a7d0b369bb37d7fefe82ac73d3b9eab2764ec0
ToxicMushroom <merlijn.verstraete@gmail.com>
2019-11-19 17:34:29 +0100

    fix

---
+++ src/main/kotlin/me/melijn/melijnbot/enums/CommandArg.kt
@@ -0,0 +1,14 @@
+package me.melijn.melijnbot.enums
+
+enum class CommandArg {
+
+    USER,
+    MEMBER,
+    TEXT_CHANNEL,
+    VOICE_CHANNEL,
+    GUILD,
+    URL,
+    ATTACHMENT,
+    COLOR,
+    STATE
+}
