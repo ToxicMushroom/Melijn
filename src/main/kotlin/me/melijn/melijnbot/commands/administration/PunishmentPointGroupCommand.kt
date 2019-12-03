2ee27dfebc1141343ad05ad9d623c1e3f4fbd275
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2019-12-03 07:36:59 +0100

    help

--- src/main/kotlin/me/melijn/melijnbot/commands/administration/PunishmentPointGroupCommand.kt
+++ src/main/kotlin/me/melijn/melijnbot/commands/administration/PunishmentPointGroupCommand.kt
@@ -1,15 +1,19 @@
 package me.melijn.melijnbot.commands.administration
 
-//class PunishmentPointGroupCommand : AbstractCommand("command.punishmentpointgroup") {
-//
-//    init {
-//        id = 124
-//        name = "punishmentPointGroup"
-//        aliases = arrayOf("ppg", "PPGroup", "punishmentPG", "punishPG")
-//        commandCategory = CommandCategory.ADMINISTRATION
-//    }
-//
-//    override suspend fun execute(context: CommandContext) {
-//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//    }
-//}
+import me.melijn.melijnbot.objects.command.AbstractCommand
+import me.melijn.melijnbot.objects.command.CommandCategory
+import me.melijn.melijnbot.objects.command.CommandContext
+
+class PunishmentPointGroupCommand : AbstractCommand("command.punishmentpointgroup") {
+
+    init {
+        id = 124
+        name = "punishmentPointGroup"
+        aliases = arrayOf("ppg", "PPGroup", "punishmentPG", "punishPG")
+        commandCategory = CommandCategory.DEVELOPER
+    }
+
+    override suspend fun execute(context: CommandContext) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
