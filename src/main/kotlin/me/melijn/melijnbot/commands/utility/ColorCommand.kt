f54129f43a9c9e3bdc9620c4dd85876aedea9cc9
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2019-12-21 18:30:34 +0100

    improvements

--- src/main/kotlin/me/melijn/melijnbot/commands/utility/ColorCommand.kt
+++ src/main/kotlin/me/melijn/melijnbot/commands/utility/ColorCommand.kt
@@ -4,9 +4,13 @@ import me.melijn.melijnbot.objects.command.AbstractCommand
 import me.melijn.melijnbot.objects.command.CommandCategory
 import me.melijn.melijnbot.objects.command.CommandContext
 import me.melijn.melijnbot.objects.embed.Embedder
-import me.melijn.melijnbot.objects.utils.*
+import me.melijn.melijnbot.objects.utils.getColorFromArgNMessage
+import me.melijn.melijnbot.objects.utils.sendEmbed
+import me.melijn.melijnbot.objects.utils.sendSyntax
+import me.melijn.melijnbot.objects.utils.toHex
 
 class ColorCommand : AbstractCommand("command.color") {
+
     init {
         id = 132
         name = "color"
@@ -20,11 +24,15 @@ class ColorCommand : AbstractCommand("command.color") {
         }
         val color = getColorFromArgNMessage(context, 0) ?: return
 
-        val fieldTitle = context.getTranslation("$root.eb.hex.title")
+        val hexTitle = context.getTranslation("$root.eb.hex.title")
+        val rgbTitle = context.getTranslation("$root.eb.rgb.title")
+        val decTitle = context.getTranslation("$root.eb.dec.title")
 
         val eb = Embedder(context)
         eb.setColor(color)
-        eb.addField(fieldTitle, color.toHex() , true)
+        eb.addField(hexTitle, color.toHex(), true)
+        eb.addField(rgbTitle, "(${color.red}, ${color.green}, ${color.blue})", true)
+        eb.addField(decTitle, color.rgb.toString(), true)
         sendEmbed(context, eb.build())
     }
 }
