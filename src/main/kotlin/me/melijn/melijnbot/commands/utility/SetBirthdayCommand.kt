ce2f714efc022d659343a16d3ec3b731e28db259
HetYoshiTeam <fabian.verstraete@gmail.com>
2020-01-25 17:00:32 +0100

    working on birthdaycommand

--- src/main/kotlin/me/melijn/melijnbot/commands/utility/SetBirthdayCommand.kt
+++ src/main/kotlin/me/melijn/melijnbot/commands/utility/SetBirthdayCommand.kt
@@ -1,36 +1,41 @@
 package me.melijn.melijnbot.commands.utility
 
-//class SetBirthdayCommand : AbstractCommand("command.setbirthday") {
-//
-//    init {
-//        id = 139
-//        name = "setBirthDay"
-//        runConditions = arrayOf(RunCondition.SUPPORTER)
-//        commandCategory = CommandCategory.UTILITY
-//    }
-//
-//    override suspend fun execute(context: CommandContext) {
-//        when {
-//            context.args.isEmpty() -> {
-//                val birthday = context.daoManager.birthdayWrapper.getBirthday(context.author.idLong)
-//                if (birthday == null) {
-//                    val name = context.getTranslation("$root.show.unset")
-//                    sendMsg(context, name)
-//                } else {
-//                    val name = context.getTranslation("$root.show.set")
-//                        .replace("%birthday%", birthday.first.toString())
-//                        .replace("%birthyear%", birthday.second.toString())
-//                    sendMsg(context, name)
-//
-//                }
-//            }
-//            context.args.size == 1 -> {
-//
-//            }
-//            else -> {
-//
-//            }
-//        }
-//    }
-//
-//}
+import me.melijn.melijnbot.objects.command.AbstractCommand
+import me.melijn.melijnbot.objects.command.CommandCategory
+import me.melijn.melijnbot.objects.command.CommandContext
+import me.melijn.melijnbot.objects.command.RunCondition
+import me.melijn.melijnbot.objects.utils.sendMsg
+
+class SetBirthdayCommand : AbstractCommand("command.setbirthday") {
+
+    init {
+        id = 139
+        name = "setBirthDay"
+        runConditions = arrayOf(RunCondition.SUPPORTER)
+        commandCategory = CommandCategory.UTILITY
+    }
+
+    override suspend fun execute(context: CommandContext) {
+        when {
+            context.args.isEmpty() -> {
+                val birthday = context.daoManager.birthdayWrapper.getBirthday(context.author.idLong)
+                if (birthday == null) {
+                    val name = context.getTranslation("$root.show.unset")
+                    sendMsg(context, name)
+                } else {
+                    val name = context.getTranslation("$root.show.set")
+                        .replace("%birthday%", birthday.first.toString())
+                        .replace("%birthyear%", birthday.second.toString())
+                    sendMsg(context, name)
+                }
+            }
+            context.args.size == 1 -> {
+
+            }
+            else -> {
+
+            }
+        }
+    }
+
+}
