ab10fa2bd13dc4ea318f6514dec231d9d74b0c96
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2020-04-27 00:05:29 +0200

    bugfix

--- src/main/kotlin/me/melijn/melijnbot/objects/events/eventlisteners/JoinLeaveListener.kt
+++ src/main/kotlin/me/melijn/melijnbot/objects/events/eventlisteners/JoinLeaveListener.kt
@@ -9,6 +9,7 @@ import me.melijn.melijnbot.objects.events.eventutil.JoinLeaveUtil.joinRole
 import me.melijn.melijnbot.objects.utils.VerificationUtils
 import me.melijn.melijnbot.objects.utils.awaitOrNull
 import me.melijn.melijnbot.objects.utils.checks.getAndVerifyChannelByType
+import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.audit.ActionType
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.events.GenericEvent
@@ -52,37 +53,42 @@ class JoinLeaveListener(container: Container) : AbstractListener(container) {
         val user = event.user
 
         if (!daoManager.unverifiedUsersWrapper.contains(event.guild.idLong, user.idLong)) {
-            val ban = event.guild.retrieveBan(user).awaitOrNull()
-            if (ban == null) {
-                val auditKick = event.guild.retrieveAuditLogs()
-                    .type(ActionType.KICK)
-                    .limit(5)
-                    .awaitOrNull()
+            if (event.guild.selfMember.hasPermission(Permission.BAN_MEMBERS, Permission.VIEW_AUDIT_LOGS)) {
+                val ban = event.guild.retrieveBan(user).awaitOrNull()
+                if (ban == null) {
+                    val auditKick = event.guild.retrieveAuditLogs()
+                        .type(ActionType.KICK)
+                        .limit(5)
+                        .awaitOrNull()
 
 
 
-                if (auditKick == null) {
-                    JoinLeaveUtil.postWelcomeMessage(daoManager, event.guild, user, ChannelType.LEAVE, MessageType.LEAVE)
-                } else {
-                    val now = OffsetDateTime.ofInstant(Instant.ofEpochMilli(System.currentTimeMillis()), ZoneId.of("GMT"))
-                    var kicked = false
-                    for (entry in auditKick) {
-                        if (entry.targetIdLong == user.idLong) {
-                            if (OffsetDateTime.now().until(now, ChronoUnit.SECONDS) < 3) {
-                                kicked = true
-                                break
+                    if (auditKick == null) {
+                        JoinLeaveUtil.postWelcomeMessage(daoManager, event.guild, user, ChannelType.LEAVE, MessageType.LEAVE)
+                    } else {
+                        val now = OffsetDateTime.ofInstant(Instant.ofEpochMilli(System.currentTimeMillis()), ZoneId.of("GMT"))
+                        var kicked = false
+                        for (entry in auditKick) {
+                            if (entry.targetIdLong == user.idLong) {
+                                if (OffsetDateTime.now().until(now, ChronoUnit.SECONDS) < 3) {
+                                    kicked = true
+                                    break
+                                }
                             }
                         }
-                    }
 
-                    if (kicked) {
-                        JoinLeaveUtil.postWelcomeMessage(daoManager, event.guild, user, ChannelType.KICKED, MessageType.KICKED)
-                    } else {
-                        JoinLeaveUtil.postWelcomeMessage(daoManager, event.guild, user, ChannelType.LEAVE, MessageType.LEAVE)
+                        if (kicked) {
+                            JoinLeaveUtil.postWelcomeMessage(daoManager, event.guild, user, ChannelType.KICKED, MessageType.KICKED)
+                        } else {
+                            JoinLeaveUtil.postWelcomeMessage(daoManager, event.guild, user, ChannelType.LEAVE, MessageType.LEAVE)
+                        }
                     }
+                } else {
+                    JoinLeaveUtil.postWelcomeMessage(daoManager, event.guild, user, ChannelType.BANNED, MessageType.BANNED)
                 }
             } else {
-                JoinLeaveUtil.postWelcomeMessage(daoManager, event.guild, user, ChannelType.BANNED, MessageType.BANNED)
+                JoinLeaveUtil.postWelcomeMessage(daoManager, event.guild, user, ChannelType.LEAVE, MessageType.LEAVE)
+
             }
 
         } else if (!guildHasNoVerification(event.guild)) {
