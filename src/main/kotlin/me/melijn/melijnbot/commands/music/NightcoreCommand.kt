f35403e0e2a29dccd899e1a0b913bdb0bfd828e4
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2020-07-20 01:20:17 +0200

    feat: NightCoreCommand

---
+++ src/main/kotlin/me/melijn/melijnbot/commands/music/NightcoreCommand.kt
@@ -0,0 +1,41 @@
+package me.melijn.melijnbot.commands.music
+
+import me.melijn.melijnbot.internals.command.AbstractCommand
+import me.melijn.melijnbot.internals.command.CommandCategory
+import me.melijn.melijnbot.internals.command.CommandContext
+import me.melijn.melijnbot.internals.command.RunCondition
+import me.melijn.melijnbot.internals.utils.getBooleanFromArgNMessage
+import me.melijn.melijnbot.internals.utils.message.sendRsp
+
+class NightcoreCommand : AbstractCommand("command.nightcore") {
+
+    init {
+        id = 196
+        name = "nightcore"
+        aliases = arrayOf("nc")
+        runConditions = arrayOf(RunCondition.VC_BOT_ALONE_OR_USER_DJ, RunCondition.PLAYING_TRACK_NOT_NULL, RunCondition.VOTED)
+        commandCategory = CommandCategory.MUSIC
+    }
+
+
+    override suspend fun execute(context: CommandContext) {
+        val state = getBooleanFromArgNMessage(context, 0) ?: return
+
+        if (state) {
+            val player = context.getGuildMusicPlayer().guildTrackManager.iPlayer
+            player.setRate(1.2)
+
+            val msg = context.getTranslation("$root.enabled")
+            sendRsp(context, msg)
+
+        } else {
+
+            val player = context.getGuildMusicPlayer().guildTrackManager.iPlayer
+            player.setRate(1.0)
+
+            val msg = context.getTranslation("$root.disabled")
+            sendRsp(context, msg)
+        }
+    }
+
+}
