81934db10a4ced4753520e4c5a3aa67dfdb91bbf
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2020-07-20 01:19:52 +0200

    feat: BassBoostCommand

---
+++ src/main/kotlin/me/melijn/melijnbot/commands/music/BassBoostCommand.kt
@@ -0,0 +1,123 @@
+package me.melijn.melijnbot.commands.music
+
+import me.melijn.melijnbot.database.audio.GainProfile
+import me.melijn.melijnbot.internals.command.AbstractCommand
+import me.melijn.melijnbot.internals.command.CommandCategory
+import me.melijn.melijnbot.internals.command.CommandContext
+import me.melijn.melijnbot.internals.command.RunCondition
+import me.melijn.melijnbot.internals.utils.getBooleanFromArgNMessage
+import me.melijn.melijnbot.internals.utils.getStringFromArgsNMessage
+import me.melijn.melijnbot.internals.utils.message.sendRsp
+import me.melijn.melijnbot.internals.utils.message.sendSyntax
+import me.melijn.melijnbot.internals.utils.withVariable
+
+
+class BassBoostCommand : AbstractCommand("command.bassboost") {
+
+    init {
+        id = 195
+        name = "bassBoost"
+        aliases = arrayOf("bb")
+        children = arrayOf(SelectArg(root))
+        runConditions = arrayOf(RunCondition.VC_BOT_ALONE_OR_USER_DJ, RunCondition.PLAYING_TRACK_NOT_NULL, RunCondition.VOTED)
+        commandCategory = CommandCategory.MUSIC
+    }
+
+
+    override suspend fun execute(context: CommandContext) {
+        val state = getBooleanFromArgNMessage(context, 0) ?: return
+
+        if (state) {
+            val id = customBass[context.guildId] ?: 0
+            val gp = bassProfiles[id]
+
+            val player = context.getGuildMusicPlayer().guildTrackManager.iPlayer
+            player.setBands(gp.toFloatArray())
+
+            val mode = context.getTranslation("$root." + when (id) {
+                0 -> "normal"
+                1 -> "light"
+                2 -> "hard"
+                3 -> "extreme"
+                else -> return
+            })
+
+            val msg = context.getTranslation("$root.enabled")
+                .withVariable("mode", mode)
+            sendRsp(context, msg)
+
+        } else {
+
+            val player = context.getGuildMusicPlayer().guildTrackManager.iPlayer
+            player.setBands(FloatArray(15) { 0.0f })
+
+            val msg = context.getTranslation("$root.disabled")
+            sendRsp(context, msg)
+        }
+    }
+
+    class SelectArg(val parent: String) : AbstractCommand("$parent.select") {
+
+        init {
+            name = "select"
+        }
+
+        override suspend fun execute(context: CommandContext) {
+            if (context.args.isEmpty()) {
+                val id = customBass[context.guildId] ?: 0
+                val mode = context.getTranslation("$parent." + when (id) {
+                    0 -> "normal"
+                    1 -> "light"
+                    2 -> "hard"
+                    3 -> "extreme"
+                    else -> return
+                })
+
+                val msg = context.getTranslation("$root.show.selected")
+                    .withVariable("mode", mode)
+                sendRsp(context, msg)
+
+            } else {
+                val selection = getStringFromArgsNMessage(context, 0, 1, 10) ?: return
+                val mode = context.getTranslation("$parent." + when (selection.toLowerCase()) {
+                    "normal" -> {
+                        customBass.remove(context.guildId)
+                        "normal"
+                    }
+                    "light" -> {
+                        customBass[context.guildId] = 1
+                        "light"
+                    }
+                    "hard" -> {
+                        customBass[context.guildId] = 2
+                        "hard"
+                    }
+                    "extreme" -> {
+                        customBass[context.guildId] = 3
+                        "extreme"
+                    }
+                    else -> {
+                        sendSyntax(context)
+                        return
+                    }
+                })
+
+                val msg = context.getTranslation("$root.set.selected")
+                    .withVariable("mode", mode)
+                sendRsp(context, msg)
+            }
+        }
+    }
+
+    companion object {
+        val customBass = mutableMapOf<Long, Int>()
+        val bassProfiles = listOf(
+            GainProfile(0.25f, 0.25f, 0.25f, 0.25f, 0.125f, 0.0625f, 0.05f, 0.025f, 0.0f, -0.025f, -0.0625f, -0.0625f, -0.0625f, -0.0625f, -0.0625f), // 0 - normal
+            GainProfile(), // 1 - light
+            GainProfile(0.4f, 0.3f, 0.15f, 0.05f, 0.0f, -0.05f, -0.1f, -0.15f, -0.15f, -0.15f, -0.15f, -0.15f, -0.15f, -0.15f, -0.15f), // 2 - harder
+            GainProfile() // 3 - hard
+        )
+    }
+
+
+}
