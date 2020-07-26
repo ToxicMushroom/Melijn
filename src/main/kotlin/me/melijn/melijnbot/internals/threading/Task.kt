824c1fb413e85a10c97ca5e0aa6d7d50f59c5816
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2020-07-26 18:17:56 +0200

    feat(logging): more tasks for better logging

--- src/main/kotlin/me/melijn/melijnbot/internals/threading/Task.kt
+++ src/main/kotlin/me/melijn/melijnbot/internals/threading/Task.kt
@@ -1,7 +1,9 @@
 package me.melijn.melijnbot.internals.threading
 
 import kotlinx.coroutines.runBlocking
+import me.melijn.melijnbot.internals.command.CommandContext
 import me.melijn.melijnbot.internals.utils.message.sendInGuild
+import net.dv8tion.jda.api.entities.*
 
 
 class Task(private val func: suspend () -> Unit) : KTRunnable {
@@ -12,6 +14,94 @@ class Task(private val func: suspend () -> Unit) : KTRunnable {
         } catch (e: Throwable) {
             e.printStackTrace()
             e.sendInGuild()
+        }
+    }
+}
+
+class MemberTask(private val member: Member, private val func: suspend () -> Unit) : KTRunnable {
+
+    override suspend fun run() {
+        try {
+            func()
+        } catch (e: Throwable) {
+            e.printStackTrace()
+            e.sendInGuild(member.guild, null, member.user)
+        }
+    }
+}
+
+class ChannelTask(private val messageChannel: MessageChannel, private val func: suspend () -> Unit) : KTRunnable {
+
+    override suspend fun run() {
+        try {
+            func()
+        } catch (e: Throwable) {
+            e.printStackTrace()
+            if (messageChannel is TextChannel) {
+                e.sendInGuild(messageChannel.guild, messageChannel)
+            } else {
+                e.sendInGuild(null, messageChannel)
+            }
+        }
+    }
+}
+
+class GuildTask(private val guild: Guild, private val func: suspend () -> Unit) : KTRunnable {
+
+    override suspend fun run() {
+        try {
+            func()
+        } catch (e: Throwable) {
+            e.printStackTrace()
+            e.sendInGuild(guild)
+        }
+    }
+}
+
+class ContextTask(private val context: CommandContext, private val func: suspend () -> Unit) : KTRunnable {
+
+    override suspend fun run() {
+        try {
+            func()
+        } catch (e: Throwable) {
+            e.printStackTrace()
+            e.sendInGuild(context)
+        }
+    }
+}
+
+class UserChannelTask(
+    private val user: User,
+    private val messageChannel: MessageChannel,
+    private val func: suspend () -> Unit
+) : KTRunnable {
+
+    override suspend fun run() {
+        try {
+            func()
+        } catch (e: Throwable) {
+            e.printStackTrace()
+            if (messageChannel is TextChannel) {
+                e.sendInGuild(messageChannel.guild, messageChannel, user)
+            } else {
+                e.sendInGuild(null, messageChannel, user)
+            }
+        }
+    }
+}
+
+class UserGuildTask(
+    private val user: User,
+    private val guild: Guild,
+    private val func: suspend () -> Unit
+) : KTRunnable {
+
+    override suspend fun run() {
+        try {
+            func()
+        } catch (e: Throwable) {
+            e.printStackTrace()
+            e.sendInGuild(guild, null, user)
         }
     }
 }
