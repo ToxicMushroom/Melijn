2cf81c5806bb37b1a8288fc3a98cb4ef50fdefcd
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2020-08-02 18:51:11 +0200

    feat: Add VoteReminderDao and DenyVoteReminderDao.kt, register wrappers in DaoManager and add /voted endpoint to rest with logging

---
+++ src/main/kotlin/me/melijn/melijnbot/internals/services/votes/VoteReminderService.kt
@@ -0,0 +1,24 @@
+package me.melijn.melijnbot.internals.services.votes
+
+import me.melijn.melijnbot.database.DaoManager
+import me.melijn.melijnbot.internals.services.Service
+import me.melijn.melijnbot.internals.threading.RunnableTask
+import me.melijn.melijnbot.internals.utils.LogUtils
+import java.util.concurrent.TimeUnit
+
+class VoteReminderService(val daoManager: DaoManager) : Service("vote-reminder", 2, 0, TimeUnit.MINUTES) {
+
+    override val service: RunnableTask = RunnableTask {
+        val cMillis = System.currentTimeMillis()
+        val votes = daoManager.voteReminderWrapper.getReminders(cMillis)
+        val denyVoteWrapper = daoManager.denyVoteReminderWrapper
+
+        for (userId in votes) {
+            val shouldRemind = denyVoteWrapper.contains(userId)
+            if (!shouldRemind) continue
+            LogUtils.sendVoteReminder(daoManager, userId)
+        }
+
+        daoManager.voteReminderWrapper.removeReminders(cMillis)
+    }
+}
