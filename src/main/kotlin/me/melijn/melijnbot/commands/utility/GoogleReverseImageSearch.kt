4f727395ac977b7a2f8c1760d142d19291607a1a
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2020-09-09 21:33:53 +0200

    try to patch gris exploitation

--- src/main/kotlin/me/melijn/melijnbot/commands/utility/GoogleReverseImageSearch.kt
+++ src/main/kotlin/me/melijn/melijnbot/commands/utility/GoogleReverseImageSearch.kt
@@ -6,6 +6,7 @@ import me.melijn.melijnbot.internals.command.CommandContext
 import me.melijn.melijnbot.internals.embed.Embedder
 import me.melijn.melijnbot.internals.utils.getImageUrlFromArgsNMessage
 import me.melijn.melijnbot.internals.utils.message.sendEmbedRsp
+import net.dv8tion.jda.api.utils.MarkdownSanitizer
 
 class GoogleReverseImageSearch : AbstractCommand("command.googlereverseimagesearch") {
 
@@ -19,7 +20,11 @@ class GoogleReverseImageSearch : AbstractCommand("command.googlereverseimagesear
     override suspend fun execute(context: CommandContext) {
         val attachment = (getImageUrlFromArgsNMessage(context, 0) ?: return).second
         val eb = Embedder(context)
-            .setDescription("[view result](https://www.google.com/searchbyimage?image_url=$attachment)")
+            .setDescription("[view result](https://www.google.com/searchbyimage?image_url=${
+                MarkdownSanitizer.escape(attachment)
+                    .replace(")", "%29")
+                    .replace("(", "%28")
+            }")
         sendEmbedRsp(context, eb.build())
     }
 }
