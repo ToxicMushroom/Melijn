6077bfcd69746068e12d0134e1ffb2f63e420158
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2020-11-04 19:22:07 +0100

    pog

--- src/main/kotlin/me/melijn/melijnbot/database/reminder/ReminderWrapper.kt
+++ src/main/kotlin/me/melijn/melijnbot/database/reminder/ReminderWrapper.kt
@@ -1,5 +1,13 @@
 package me.melijn.melijnbot.database.reminder
 
+import me.melijn.melijnbot.database.votes.VoteReminder
+
 class ReminderWrapper(val reminderDao: ReminderDao) {
-    fun add()
+    fun add(reminder: VoteReminder) {
+
+    }
+
+    fun remove(userId: Long, time: Long) {
+        reminderDao.remove(userId, time)
+    }
 }
