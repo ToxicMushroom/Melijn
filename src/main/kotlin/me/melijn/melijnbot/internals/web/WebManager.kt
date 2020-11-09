643c18644a19becfc2791386c1ba3d7b7b4ac471
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2020-11-09 09:31:19 +0100

    feat: Rule34Command.kt

--- src/main/kotlin/me/melijn/melijnbot/internals/web/WebManager.kt
+++ src/main/kotlin/me/melijn/melijnbot/internals/web/WebManager.kt
@@ -8,6 +8,7 @@ import me.melijn.melijnbot.internals.Settings
 import me.melijn.melijnbot.internals.web.bins.BinApis
 import me.melijn.melijnbot.internals.web.botlist.BotListApi
 import me.melijn.melijnbot.internals.web.kitsu.KitsuApi
+import me.melijn.melijnbot.internals.web.nsfw.Rule34Api
 import me.melijn.melijnbot.internals.web.osu.OsuApi
 import me.melijn.melijnbot.internals.web.spotify.MySpotifyApi
 import me.melijn.melijnbot.internals.web.weebsh.WeebshApi
@@ -29,14 +30,15 @@ class WebManager(val settings: Settings) {
             this.preconfigured = client
         }
     }
-
     val aniListApolloClient: ApolloClient = ApolloClient.builder()
         .serverUrl("https://graphql.anilist.co")
         .okHttpClient(OkHttpClient())
         .build()
 
-
     var spotifyApi: MySpotifyApi? = null
+
+
+    val rule34: Rule34Api = Rule34Api(httpClient)
     val binApis: BinApis = BinApis(httpClient)
     val kitsuApi: KitsuApi = KitsuApi(httpClient)
     val osuApi: OsuApi = OsuApi(proxiedHttpClient, settings.tokens.osu)
