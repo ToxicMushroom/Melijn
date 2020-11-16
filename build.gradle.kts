cc56ac92348eeafd111cb020b4a41c342144cf6d
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2020-11-16 12:39:00 +0100

    chore(deps): bump jda

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ repositories {
 
 dependencies {
     // https://bintray.com/dv8fromtheworld/maven/JDA/
-    implementation("net.dv8tion:JDA:4.2.0_216") {
+    implementation("net.dv8tion:JDA:4.2.0_217") {
         exclude("opus-java")
     }
 
