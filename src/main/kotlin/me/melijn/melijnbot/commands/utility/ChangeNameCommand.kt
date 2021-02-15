890d682f240b2ee530a969d1e1947e8b7439af7b
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2021-02-15 13:08:03 +0100

    fix: handle edge cases, handle permission error feat: send response

--- src/main/kotlin/me/melijn/melijnbot/commands/utility/ChangeNameCommand.kt
+++ src/main/kotlin/me/melijn/melijnbot/commands/utility/ChangeNameCommand.kt
@@ -3,10 +3,12 @@ package me.melijn.melijnbot.commands.utility
 import me.melijn.melijnbot.internals.command.AbstractCommand
 import me.melijn.melijnbot.internals.command.CommandCategory
 import me.melijn.melijnbot.internals.command.ICommandContext
-import me.melijn.melijnbot.internals.utils.await
-import me.melijn.melijnbot.internals.utils.getStringFromArgsNMessage
+import me.melijn.melijnbot.internals.translation.MESSAGE_SELFINTERACT_MEMBER_HIARCHYEXCEPTION
+import me.melijn.melijnbot.internals.translation.PLACEHOLDER_USER
+import me.melijn.melijnbot.internals.utils.*
+import me.melijn.melijnbot.internals.utils.message.sendRsp
 import me.melijn.melijnbot.internals.utils.message.sendSyntax
-import me.melijn.melijnbot.internals.utils.retrieveMemberByArgsNMessage
+import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.Member
 
 class ChangeNameCommand : AbstractCommand("command.changename") {
@@ -14,38 +16,51 @@ class ChangeNameCommand : AbstractCommand("command.changename") {
     init {
         id = 249
         name = "changeName"
-        aliases = arrayOf("cg", "nickname")
+        aliases = arrayOf("nickname", "nick")
+        discordPermissions = arrayOf(Permission.NICKNAME_MANAGE)
         commandCategory = CommandCategory.UTILITY
     }
 
     override suspend fun execute(context: ICommandContext) {
+        if (context.args.isEmpty()) {
+            sendSyntax(context)
+            return
+        }
+
         val member: Member = if (context.args.size == 1) {
             context.member
-        }
-        else {
+        } else {
             retrieveMemberByArgsNMessage(context, 0) ?: return
         }
-
-        val newNickname = if (context.args.size == 1) {
-            getStringFromArgsNMessage(context, 0, 0, 32)
+        if (!context.selfMember.canInteract(member)) {
+            val msg = context.getTranslation(MESSAGE_SELFINTERACT_MEMBER_HIARCHYEXCEPTION)
+                .withSafeVariable(PLACEHOLDER_USER, member.asTag)
+            sendRsp(context, msg)
+            return
         }
-        else {
-            if (context.args.size > 2){
-                return sendSyntax(context)
-            }
-            else{
-                if (context.args.isEmpty()){
-                    ""
-                }
-                else {
-                    getStringFromArgsNMessage(context, 1, 0, 32)
-                }
-            }
-        }
-            member.modifyNickname(newNickname).await()
 
+        val newNickname = getStringFromArgsNMessage(context, context.args.size - 1, 0, 32) ?: return
+        val msg = if (newNickname == "-reset") {
+            member.modifyNickname(newNickname).reason("(changeName) ${context.author.asTag}").await()
+
+            if (member.idLong == context.member.idLong) {
+                context.getTranslation("$root.reset.self")
+            } else {
+                context.getTranslation("$root.reset.other")
+                    .withSafeVariable("user", member.asTag)
+            }.withSafeVariable("newName", newNickname)
+        } else {
+            member.modifyNickname(null).reason("(changeName) ${context.author.asTag}").await()
+
+            if (member.idLong == context.member.idLong) {
+                context.getTranslation("$root.changed.self")
+            } else {
+                context.getTranslation("$root.changed.other")
+                    .withSafeVariable("user", member.asTag)
+            }.withSafeVariable("newName", newNickname)
+        }
+
+        sendRsp(context, msg)
     }
-
-
 }
 
