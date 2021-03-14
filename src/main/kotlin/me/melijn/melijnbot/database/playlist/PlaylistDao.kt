3444faa40c79919b85c8ef846eeb2cc10a0ab4b1
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2021-03-14 17:35:24 +0100

    feat: playlist rename fix: full name support for playlist indexes

--- src/main/kotlin/me/melijn/melijnbot/database/playlist/PlaylistDao.kt
+++ src/main/kotlin/me/melijn/melijnbot/database/playlist/PlaylistDao.kt
@@ -18,18 +18,24 @@ class PlaylistDao(driverManager: DriverManager) : CacheDBDao(driverManager) {
 
 
     fun set(userId: Long, playlist: String, id: Int, track: String) {
-        driverManager.executeUpdate("INSERT INTO $table (userId, playlist, id, track) VALUES (?, ?, ?, ?) ON CONFLICT ($primaryKey) DO UPDATE SET track = ?",
-            userId, playlist, id, track, track)
+        driverManager.executeUpdate(
+            "INSERT INTO $table (userId, playlist, id, track) VALUES (?, ?, ?, ?) ON CONFLICT ($primaryKey) DO UPDATE SET track = ?",
+            userId, playlist, id, track, track
+        )
     }
 
     fun removeById(userId: Long, playlist: String, id: Int) {
-        driverManager.executeUpdate("DELETE FROM $table WHERE userId = ? AND playlist = ? AND id = ?",
-            userId, playlist, id)
+        driverManager.executeUpdate(
+            "DELETE FROM $table WHERE userId = ? AND playlist = ? AND id = ?",
+            userId, playlist, id
+        )
     }
 
     fun removeByTrack(userId: Long, playlist: String, track: String) {
-        driverManager.executeUpdate("DELETE FROM $table WHERE userId = ? AND playlist = ? AND track = ?",
-            userId, playlist, track)
+        driverManager.executeUpdate(
+            "DELETE FROM $table WHERE userId = ? AND playlist = ? AND track = ?",
+            userId, playlist, track
+        )
     }
 
 
@@ -58,20 +64,27 @@ class PlaylistDao(driverManager: DriverManager) : CacheDBDao(driverManager) {
 
     fun removeByIds(userId: Long, playlist: String, positions: List<Int>) {
         driverManager.getUsableConnection { con ->
-            con.prepareStatement("DELETE FROM $table WHERE userId = ? AND playlist = ? AND id = ?").use { preparedStatement ->
-                preparedStatement.setLong(1, userId)
-                preparedStatement.setString(2, playlist)
-                for (id in positions) {
-                    preparedStatement.setInt(3, id)
-                    preparedStatement.addBatch()
+            con.prepareStatement("DELETE FROM $table WHERE userId = ? AND playlist = ? AND id = ?")
+                .use { preparedStatement ->
+                    preparedStatement.setLong(1, userId)
+                    preparedStatement.setString(2, playlist)
+                    for (id in positions) {
+                        preparedStatement.setInt(3, id)
+                        preparedStatement.addBatch()
+                    }
+                    preparedStatement.executeBatch()
                 }
-                preparedStatement.executeBatch()
-            }
         }
     }
 
     fun clear(userId: Long, playlist: String) {
-        driverManager.executeUpdate("DELETE FROM $table WHERE userId = ? AND playlist = ?",
-            userId, playlist)
+        driverManager.executeUpdate(
+            "DELETE FROM $table WHERE userId = ? AND playlist = ?",
+            userId, playlist
+        )
+    }
+
+    fun rename(previousName: String, newName: String) {
+        driverManager.executeUpdate("UPDATE $table SET playlist = ? WHERE playlist = ?", newName, previousName)
     }
 }
