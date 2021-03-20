22773f2aa1543d4091503edf2bf4b43c3e65cf4c
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2021-03-20 14:59:01 +0100

    feat: migration script for messages and custom commands

--- src/main/kotlin/me/melijn/melijnbot/commands/developer/TestCommand.kt
+++ src/main/kotlin/me/melijn/melijnbot/commands/developer/TestCommand.kt
@@ -1,11 +1,13 @@
 package me.melijn.melijnbot.commands.developer
 
+import me.melijn.melijnbot.database.message.ModularMessage
+import me.melijn.melijnbot.enums.MessageType
 import me.melijn.melijnbot.internals.command.AbstractCommand
 import me.melijn.melijnbot.internals.command.CommandCategory
 import me.melijn.melijnbot.internals.command.ICommandContext
+import me.melijn.melijnbot.internals.utils.toUCC
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-
 
 class TestCommand : AbstractCommand("command.test") {
 
@@ -18,7 +20,71 @@ class TestCommand : AbstractCommand("command.test") {
     val logger: Logger = LoggerFactory.getLogger(TestCommand::class.java)
 
     override suspend fun execute(context: ICommandContext) {
+        val driver = context.daoManager.driverManager
+        var counter = 0
+        driver.executeQuery("SELECT * FROM messages_old", { rs ->
+            while (rs.next()) {
+                val guildId = rs.getLong("guildId")
+                val typeStr = rs.getString("type")
+                val type = try {
+                    MessageType.valueOf(typeStr)
+                } catch (t: Throwable) {
+                    logger.warn("Unknown message type: $typeStr")
+                    null
+                }
+                if (type != null) {
+                    val message = rs.getString("message")
+                    val modular = try {
+                        ModularMessage.fromJSON(message)
+                    } catch (t: Throwable) {
+                        null
+                    }
+                    if (modular == null) logger.warn("Failed to parse message: $message")
+                    else {
+                        val msgName = type.toUCC()
+                        context.daoManager.messageWrapper.setMessage(guildId, msgName, modular)
+                        context.daoManager.linkedMessageWrapper.setMessage(guildId, type, msgName)
+                        logger.info("Migrated row: ${++counter} :) of messages")
+                    }
+                }
+            }
 
-        context.reply("nothing here")
+            context.reply("Migrated messages")
+        })
+
+        var counter2 = 0
+        driver.executeQuery("SELECT * FROM customcommands", { rs ->
+            while (rs.next()) {
+                val guildId = rs.getLong("guildId")
+                val id = rs.getLong("id")
+                val chance = rs.getInt("chance")
+                val prefix = rs.getBoolean("prefix")
+
+                val name = rs.getString("name")
+                val description = rs.getString("description")
+                val aliases = rs.getString("aliases")
+                val message = rs.getString("content")
+                val modular = try {
+                    ModularMessage.fromJSON(message)
+                } catch (t: Throwable) {
+                    null
+                }
+                if (modular == null) logger.warn("Failed to parse cc-message: $message")
+                else {
+                    val msgName = "cc.$id"
+                    context.daoManager.messageWrapper.setMessage(guildId, msgName, modular)
+
+                    driver.executeUpdate(
+                        "INSERT INTO custom_commands (guild_id, name, description, msg_name, aliases, chance, prefix, contains_triggers) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                        guildId, name, description, msgName, aliases, chance, prefix, false
+                    )
+                    logger.info("Migrated row: ${++counter2} :) of customcommands")
+                }
+
+            }
+            context.reply("Migrated custom commands")
+        })
+
+        context.reply("Migrating, pog pog")
     }
 }
