0959904ffafe6a2c1a6509fe55d08feee2367fe7
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2021-05-04 17:44:16 +0200

    feat: ready probe

--- src/main/kotlin/me/melijn/melijnbot/internals/web/ProbeServer.kt
+++ src/main/kotlin/me/melijn/melijnbot/internals/web/ProbeServer.kt
@@ -1,11 +1,15 @@
 package me.melijn.melijnbot.internals.web
 
 import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import me.melijn.melijnbot.Container
+import me.melijn.melijnbot.MelijnBot
 import me.melijn.melijnbot.internals.web.rest.shutdown.ShutdownResponseHandler
+import net.dv8tion.jda.api.JDA
 import java.util.concurrent.TimeUnit
 
 class ProbeServer(container: Container) {
@@ -15,6 +19,13 @@ class ProbeServer(container: Container) {
             get("/shutdown") {
                 ShutdownResponseHandler.handleShutdownResponse(RequestContext(call, container), false)
                 stop()
+            }
+            get("/ready") {
+                if (MelijnBot.shardManager.shards.all { it.status == JDA.Status.CONNECTED || it.status == JDA.Status.LOADING_SUBSYSTEMS }) {
+                    call.respondText(status = HttpStatusCode.OK) { "ready" }
+                } else {
+                    call.respondText(status = HttpStatusCode.InternalServerError) { "no ready" }
+                }
             }
         }
     }
