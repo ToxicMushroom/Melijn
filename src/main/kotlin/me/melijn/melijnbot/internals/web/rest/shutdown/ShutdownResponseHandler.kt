f54e23daf09f6014334b14d924b2fcd865acd6d3
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2021-05-20 20:53:16 +0200

    refactor: improve shutdown flow

--- src/main/kotlin/me/melijn/melijnbot/internals/web/rest/shutdown/ShutdownResponseHandler.kt
+++ src/main/kotlin/me/melijn/melijnbot/internals/web/rest/shutdown/ShutdownResponseHandler.kt
@@ -3,7 +3,9 @@ package me.melijn.melijnbot.internals.web.rest.shutdown
 import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
+import kotlinx.coroutines.delay
 import me.melijn.melijnbot.MelijnBot
+import me.melijn.melijnbot.internals.threading.TaskManager
 import me.melijn.melijnbot.internals.web.RequestContext
 
 object ShutdownResponseHandler {
@@ -14,11 +16,13 @@ object ShutdownResponseHandler {
         val wrapper = container.daoManager.tracksWrapper
 
         if (reqAuth && call.request.header("Authorization") != context.restToken) {
+            println("INVALID TOKEN SHUTDOWN")
             call.respondText(status = HttpStatusCode.Forbidden) { "Invalid token\n" }
             return
         }
 
-        container.shuttingDown = true
+        container.shuttingDown = true // stops services, and updates status
+        container.restServer.stop()
 
         for ((guildId, player) in HashMap(players)) {
             val guild = MelijnBot.shardManager.getGuildById(guildId) ?: continue
@@ -32,6 +36,16 @@ object ShutdownResponseHandler {
             wrapper.addChannel(guildId, channel.idLong)
 
             trackManager.stopAndDestroy()
+        }
+
+        val job = TaskManager.async {
+            MelijnBot.shardManager.shutdown()
+        }
+
+        var seconds = 10
+        while (!job.isCompleted && seconds != 0) {
+            delay(1000)
+            seconds--
         }
 
         call.respondText { "Shutdown complete!" }
