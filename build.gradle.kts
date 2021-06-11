6bc453244b081c94a7d00fb037a38c49fd073808
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2021-06-11 12:34:50 +0200

    chore(deps): bump jda

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ val scrimage = "4.2.0.92-SNAPSHOT"
 
 dependencies {
     // https://ci.dv8tion.net/job/JDA/
-    implementation("net.dv8tion:JDA:4.2.1_276") {
+    implementation("net.dv8tion:JDA:4.3.0_277") {
         exclude("opus-java")
     }
 
