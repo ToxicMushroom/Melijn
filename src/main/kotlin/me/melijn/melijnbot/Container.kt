60edaadb38295c9bc1fcdefb920c605c3c8d68b6
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2021-10-09 02:55:05 +0200

    fix: not receiving votes

--- src/main/kotlin/me/melijn/melijnbot/Container.kt
+++ src/main/kotlin/me/melijn/melijnbot/Container.kt
@@ -29,7 +29,7 @@ val objectMapper: ObjectMapper = jacksonObjectMapper()
 
 class Container {
 
-    var voteReq: Boolean = true
+    var voteReq: Boolean = false
     var logToDiscord: Boolean = true
     lateinit var podInfo: PodInfo
 
