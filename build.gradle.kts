9ef6852798c76b024882622085150e5dab4e42d9
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2021-10-14 17:41:08 +0200

    chore: deps bump shadowjar

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("application")
     id("com.apollographql.apollo") version "2.5.9"
-    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
     kotlin("jvm") version "1.5.31"
 }
 
