ac74e38d1fe061a26681a93b53ed9c5ca47ebc97
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2022-03-07 10:08:57 +0100

    feat: filter bannable discord tags from board sites

--- src/main/kotlin/me/melijn/melijnbot/internals/web/apis/BooruApi.kt
+++ src/main/kotlin/me/melijn/melijnbot/internals/web/apis/BooruApi.kt
@@ -21,13 +21,21 @@ class BooruApi(val httpClient: HttpClient) {
         .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
+    val bannableDiscordItems = listOf(" loli ", "lolita", " cub ", " shota ")
+
     suspend fun getRandomPost(domain: String, tags: String): BooruEntry? {
         val response = httpClient.get<String>("https://$domain/index.php?page=dapi&s=post&q=index") {
             this.parameter("tags", tags)
         }
 
         return try {
-            val legal = kotlinXmlMapper.readValue<BooruResponse>(response).posts.filter { !it.hasChildren }
+            val legal = kotlinXmlMapper.readValue<BooruResponse>(response).posts
+                .filter { !it.hasChildren }
+                .filter { bannableDiscordItems
+                    .all { banned ->
+                        !it.tags.contains(banned)
+                    }
+                }
             legal[Random.nextInt(legal.size)]
         } catch (t: Throwable) {
             null
@@ -48,5 +56,7 @@ data class BooruEntry(
     @JacksonXmlProperty(isAttribute = true, localName = "file_url")
     var imageUrl: String,
     @JacksonXmlProperty(isAttribute = true, localName = "has_children")
-    var hasChildren: Boolean
+    var hasChildren: Boolean,
+    @JacksonXmlProperty(isAttribute = true, localName = "tags")
+    var tags: String
 )
