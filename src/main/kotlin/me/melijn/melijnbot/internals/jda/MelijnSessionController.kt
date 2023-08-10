612c05876d5324c0982e35a24c7baf5822dd6818
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2023-08-10 15:44:15 +0200

    feat: stop using redis for concurrency, not needed

--- src/main/kotlin/me/melijn/melijnbot/internals/jda/MelijnSessionController.kt
+++ src/main/kotlin/me/melijn/melijnbot/internals/jda/MelijnSessionController.kt
@@ -1,6 +1,5 @@
 package me.melijn.melijnbot.internals.jda
 
-import kotlinx.coroutines.runBlocking
 import me.melijn.melijnbot.database.ratelimit.RatelimitWrapper
 import net.dv8tion.jda.api.utils.ConcurrentSessionController
 
@@ -12,24 +11,24 @@ class MelijnSessionController(private val ratelimitWrapper: RatelimitWrapper) : 
         setConcurrency(16)
     }
 
-    override fun getGlobalRatelimit(): Long {
-        return runBlocking {
-            try {
-                ratelimitWrapper.get() ?: internalRatelimit
-            } catch (t: Throwable) {
-                internalRatelimit
-            }
-        }
-    }
-
-    override fun setGlobalRatelimit(ratelimit: Long) {
-        return runBlocking {
-            internalRatelimit = ratelimit
-            try {
-                ratelimitWrapper.set(ratelimit)
-            } catch (t: Throwable) {
-                t.printStackTrace()
-            }
-        }
-    }
+//    override fun getGlobalRatelimit(): Long {
+//        return runBlocking {
+//            try {
+//                ratelimitWrapper.get() ?: internalRatelimit
+//            } catch (t: Throwable) {
+//                internalRatelimit
+//            }
+//        }
+//    }
+//
+//    override fun setGlobalRatelimit(ratelimit: Long) {
+//        return runBlocking {
+//            internalRatelimit = ratelimit
+//            try {
+//                ratelimitWrapper.set(ratelimit)
+//            } catch (t: Throwable) {
+//                t.printStackTrace()
+//            }
+//        }
+//    }
 }
